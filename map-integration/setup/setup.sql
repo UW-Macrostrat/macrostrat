3f2d7d52273e15dcd23ef9f915f64dcbf865e724
Shanan Peters <cambro@gmail.com>
2015-07-03 15:13:33 -0500

    updated sources definition

--- map-integration/setup/setup.sql
+++ map-integration/setup/setup.sql
@@ -71,7 +71,13 @@ CREATE TABLE maps.large (
 CREATE TABLE maps.sources (
   source_id serial,
   name varchar(255),
-  primary_table varchar(255)
+  primary_table varchar(50),
+  url varchar(255),
+  ref_title text,
+  authors varchar(255),
+  ref_year smallint,
+  ref_source varchar(255),
+  isbn_doi varchar(100)
 );
 
 CREATE TABLE maps.map_liths (
