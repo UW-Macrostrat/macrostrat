be454869e5c5906b77f5f804c612b01a845a940c
John J Czaplewski <jczaplew@gmail.com>
2015-07-16 17:38:13 -0500

    Add materialized view building process for tiles

---
+++ map-integration/tiles/setup.sql
@@ -0,0 +1,209 @@
+CREATE MATERIALIZED VIEW medium_map AS
+WITH start AS (
+	SELECT source_id, st_extent(geom)::geometry extent
+	FROM maps.medium
+	GROUP BY source_id
+),
+list AS (
+	SELECT a.source_id s1, b.source_id s2, st_intersects(a.extent, b.extent) intersects
+	FROM start a, start b
+	ORDER BY a.source_id
+),
+relationships AS (
+	select s1, s2 FROM list WHERE intersects IS true
+),
+summary AS (
+	SELECT s1 AS name, array_agg(s2) AS touches
+	FROM relationships
+	GROUP BY s1
+),
+grouped AS (
+	SELECT name as source_id, (
+		SELECT array_agg(uniques) FROM (
+			select distinct unnest(array_agg_mult(sub.touches)) AS uniques
+			ORDER BY uniques
+		) x
+	) my_group
+
+	FROM summary LEFT JOIN LATERAL (
+	  SELECT touches
+	  FROM summary r
+	  WHERE summary.touches && r.touches
+	  GROUP BY name, touches
+	) sub ON true
+	GROUP BY summary.name
+	ORDER BY summary.name
+),
+
+organized AS (
+  SELECT DISTINCT my_group, row_number() over() as group_id
+  FROM grouped
+  GROUP BY my_group
+),
+
+parsed AS (
+  SELECT unnest(my_group) AS source_id, group_id FROM organized
+)
+
+SELECT
+  map_id,
+  medium.source_id,
+  geom,
+  (select interval_color from macrostrat.intervals where age_top <= ti.age_top AND age_bottom >= tb.age_bottom order by age_bottom - age_top limit 1) color,
+  group_id
+FROM maps.medium
+JOIN macrostrat.intervals ti ON ti.id = t_interval
+JOIN macrostrat.intervals tb ON tb.id = b_interval
+JOIN parsed ON parsed.source_id = medium.source_id;
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE MATERIALIZED VIEW small_map AS
+WITH start AS (
+	SELECT source_id, st_extent(geom)::geometry extent
+	FROM maps.small
+	GROUP BY source_id
+),
+
+list AS (
+	SELECT a.source_id s1, b.source_id s2, st_intersects(a.extent, b.extent) intersects
+	FROM start a, start b
+	ORDER BY a.source_id
+),
+
+relationships AS (
+	select s1, s2 FROM list WHERE intersects IS true
+),
+
+summary AS (
+	SELECT s1 AS name, array_agg(s2) AS touches
+	FROM relationships
+	GROUP BY s1
+),
+
+grouped AS (
+	SELECT name as source_id, (
+		SELECT array_agg(uniques) FROM (
+			select distinct unnest(array_agg_mult(sub.touches)) AS uniques
+			ORDER BY uniques
+		) x
+	) my_group
+
+	FROM summary LEFT JOIN LATERAL (
+	  SELECT touches
+	  FROM summary r
+	  WHERE summary.touches && r.touches
+	  GROUP BY name, touches
+	) sub ON true
+	GROUP BY summary.name
+	ORDER BY summary.name
+),
+
+organized AS (
+  SELECT DISTINCT my_group, row_number() over() as group_id
+  FROM grouped
+  GROUP BY my_group
+),
+
+parsed AS (
+  SELECT unnest(my_group) AS source_id, group_id FROM organized
+)
+
+SELECT
+  map_id,
+  medium.source_id,
+  geom,
+  (select interval_color from macrostrat.intervals where age_top <= ti.age_top AND age_bottom >= tb.age_bottom order by age_bottom - age_top limit 1) color,
+  group_id
+FROM maps.medium
+JOIN macrostrat.intervals ti ON ti.id = t_interval
+JOIN macrostrat.intervals tb ON tb.id = b_interval
+JOIN parsed ON parsed.source_id = medium.source_id;
+
+
+
+
+
+
+CREATE MATERIALIZED VIEW large_map AS
+WITH start AS (
+	SELECT source_id, st_extent(geom)::geometry extent
+	FROM maps.large
+	GROUP BY source_id
+),
+
+list AS (
+	SELECT a.source_id s1, b.source_id s2, st_intersects(a.extent, b.extent) intersects
+	FROM start a, start b
+	ORDER BY a.source_id
+),
+
+relationships AS (
+	select s1, s2 FROM list WHERE intersects IS true
+),
+
+summary AS (
+	SELECT s1 AS name, array_agg(s2) AS touches
+	FROM relationships
+	GROUP BY s1
+),
+
+grouped AS (
+	SELECT name as source_id, (
+		SELECT array_agg(uniques) FROM (
+			select distinct unnest(array_agg_mult(sub.touches)) AS uniques
+			ORDER BY uniques
+		) x
+	) my_group
+
+	FROM summary LEFT JOIN LATERAL (
+	  SELECT touches
+	  FROM summary r
+	  WHERE summary.touches && r.touches
+	  GROUP BY name, touches
+	) sub ON true
+	GROUP BY summary.name
+	ORDER BY summary.name
+),
+
+organized AS (
+  SELECT DISTINCT my_group, row_number() over() as group_id
+  FROM grouped
+  GROUP BY my_group
+),
+
+parsed AS (
+  SELECT unnest(my_group) AS source_id, group_id FROM organized
+)
+
+SELECT
+  map_id,
+  medium.source_id,
+  geom,
+  (select interval_color from macrostrat.intervals where age_top <= ti.age_top AND age_bottom >= tb.age_bottom order by age_bottom - age_top limit 1) color,
+  group_id
+FROM maps.medium
+JOIN macrostrat.intervals ti ON ti.id = t_interval
+JOIN macrostrat.intervals tb ON tb.id = b_interval
+JOIN parsed ON parsed.source_id = medium.source_id;
+
+
+
+
+CREATE INDEX medium_map_source_idx ON medium_map (source_id);
+CREATE INDEX medium_map_geom_idx ON medium_map USING GIST (geom);
+
+CREATE INDEX small_map_source_idx ON small_map (source_id);
+CREATE INDEX small_map_geom_idx ON small_map USING GIST (geom);
+
+CREATE INDEX large_map_source_idx ON large_map (source_id);
+CREATE INDEX large_map_geom_idx ON large_map USING GIST (geom);
