5232713b452aedb11fa3ebc8e6e6de09a6470353
John J Czaplewski <jczaplew@gmail.com>
2015-09-10 12:56:03 -0500

    Fix error in matching scripts that ignored name hierarchy

--- map-integration/matching/match_multiprocessor.py
+++ map-integration/matching/match_multiprocessor.py
@@ -58,13 +58,36 @@ class Task(object):
               JOIN macrostrat.intervals intervals_bottom on b_interval = intervals_bottom.id
               WHERE source_id = %(source_id)s
      ),
-     macro AS (SELECT us.unit_id AS unit_id, lsn.strat_name_id, lsn.strat_name, c.poly_geom, lui.lo_age as age_top, lui.fo_age as age_bottom
-              FROM macrostrat.units_sections us
-              JOIN macrostrat.lookup_unit_intervals lui ON us.unit_id = lui.unit_id
-              JOIN macrostrat.unit_strat_names usn ON us.unit_id = usn.unit_id
-              JOIN macrostrat.lookup_strat_names lsn ON usn.strat_name_id = lsn.strat_name_id
-              JOIN macrostrat.cols c ON us.col_id = c.id
-              WHERE c.status_code = 'active'
+     macro AS (
+        WITH first AS (
+            SELECT DISTINCT lsn.strat_name_id,
+              unnest(array(
+                SELECT not_null_names FROM (
+                  SELECT unnest(
+                    array[lsn.bed_name, lsn.mbr_name, lsn.fm_name, lsn.gp_name, lsn.sgp_name]
+                  ) not_null_names
+                ) sub WHERE not_null_names IS NOT NULL
+              )) AS names,
+
+              unnest(array(
+                SELECT not_null_ids FROM (
+                  SELECT unnest(
+                    array[lsn.bed_id, lsn.mbr_id, lsn.fm_id, lsn.gp_id, lsn.sgp_id]
+                  ) not_null_ids
+                ) sub WHERE not_null_ids != 0
+              )) AS strat_name_ids
+            FROM macrostrat.lookup_strat_names lsn
+            JOIN macrostrat.unit_strat_names usn ON usn.strat_name_id = lsn.strat_name_id
+            ORDER BY lsn.strat_name_id
+          )
+
+          SELECT units.id AS unit_id, first.names AS strat_name, first.strat_name_ids AS strat_name_id, lui.lo_age AS age_top, lui.fo_age AS age_bottom, poly_geom
+          FROM macrostrat.units
+          JOIN macrostrat.unit_strat_names usn ON units.id = usn.unit_id
+          JOIN first ON first.strat_name_id = usn.strat_name_id
+          JOIN macrostrat.cols ON cols.id = units.col_id
+          JOIN macrostrat.lookup_unit_intervals lui ON units.id = lui.unit_id
+          WHERE cols.status_code = 'active'
      )
     SELECT DISTINCT rocks.map_id, macro.unit_id, macro.strat_name_id FROM rocks, macro
     WHERE macro.strat_name != ''
