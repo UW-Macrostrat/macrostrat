30290dd1fe533ade45631b2cef6a83210f1ef585
John J Czaplewski <jczaplew@gmail.com>
2015-09-30 16:23:39 -0500

    Better tile seeding

--- map-integration/tiles/create_config.py
+++ map-integration/tiles/create_config.py
@@ -1,32 +1,11 @@
 import sys, os
 import psycopg2
 from psycopg2.extensions import AsIs
-import argparse
 import copy
 import json
-import multiprocessing
 from subprocess import call, check_call, CalledProcessError
 import subprocess
 
-parser = argparse.ArgumentParser(
-  description="(re)roll tiles for burwell",
-  epilog="Example usage: python generate-tiles.py --all")
-
-parser.add_argument("--source_id", dest="source_id",
-  type=int, required=False,
-  help="The ID of the desired source to generate")
-
-parser.add_argument("--scale", dest="scale",
-  type=str, required=False,
-  help="The scale to regenerate. Can be 'tiny', 'small', 'medium', or 'large'.")
-
-parser.add_argument("--all", dest="all",
-  required=False, action="store_true",
-  help="Regenerate all tiles")
-
-arguments = parser.parse_args()
-
-cpus = multiprocessing.cpu_count() - 2
 
 # Burwell_tiny = tiny [0, 1, 2, 3, 4]
 # Burwell_small = small, medium, large [5, 6]
@@ -35,12 +14,6 @@ cpus = multiprocessing.cpu_count() - 2
 
 # For each, build a new project file, using the same stylesheet
 
-def return_help():
-    parser.print_help()
-    sys.exit(0)
-
-if not arguments.all and not arguments.source_id and not arguments.scale:
-    return_help()
 
 # Connect to the database
 try:
@@ -135,16 +108,7 @@ def create_layer(scale) :
     return layer
 
 
-def clean_up_tmp():
-    try:
-        check_call("rm -rf tmp", shell=True)
-    except CalledProcessError:
-        print "Error deleting folder tmp"
-        sys.exit()
-
 def setup():
-    clean_up_tmp()
-
     print "--- Building styles.mss ---"
     # First, rebuild the file `styles.mss` in the event any colors were changed
     cur.execute("""
@@ -236,164 +200,15 @@ def setup():
         # Use kosmtik top convert the project file to a Mapnik XML file that can be read by TileStache
         call(["node", "node_modules/kosmtik/index.js", "export", "burwell_" + scale + ".mml", "--format", "xml", "--output", "burwell_" + scale + ".xml", "--minZoom", min(scale_map[scale]), "--max_zoom", max(scale_map[scale])])
 
-        #call(["cp", "burwell_" + scale + ".xml", "TileStache/burwell_" + scale + ".xml"])
-
 
 def clean_up():
     try:
-        check_call("rm burwell_large.* && rm burwell_medium.* && rm burwell_small.* && rm burwell_tiny.* && rm -rf tmp", shell=True)
+        check_call("rm burwell_large.mml && rm burwell_medium.mml && rm burwell_small.mml && rm burwell_tiny.mml && rm -rf tmp", shell=True)
     except CalledProcessError:
         print "Error cleaning up files"
         sys.exit()
 
 
-def find_groups(scale, source_id=None):
-    params = {
-      "scale": AsIs(scale)
-    }
-
-    pre = ""
-    where = ""
-
-    # If a source_id is supplied, find the group_id that it belongs to
-    if source_id:
-        pre = "WITH the_group AS (SELECT DISTINCT group_id FROM lookup_%(scale)s x JOIN maps.%(scale)s s ON s.map_id = x.map_id WHERE s.source_id = %(source_id)s LIMIT 1)"
-        where = " WHERE group_id IN (SELECT * FROM the_group)"
-        params["source_id"] = source_id
-
-    # Get the extent of the target group(s)
-    cur.execute(pre + """
-        SELECT group_id, ST_Extent(geom) AS extent
-        FROM lookup_%(scale)s x
-        JOIN maps.%(scale)s s ON s.map_id = x.map_id
-        """ + where + """
-        GROUP BY group_id
-    """, params)
-    groups = cur.fetchall()
-
-    modified_groups = []
-    for group in groups:
-        # Extent comes back like BOX(min_lng, min_lat, max_lng, max_lat), but...
-        # we need [min_lat, min_lng, max_lat, max_lng]
-        clean_extent = group[1].replace("BOX(", "").replace(")", "").split(",")
-        min_coords = clean_extent[0].split(" ")
-        max_coords = clean_extent[1].split(" ")
-
-        modified_groups.append({
-            "group_id": group[0],
-            "extent": [min_coords[1], min_coords[0], max_coords[1], max_coords[0]]
-        })
-
-    # Return an array of objects, where each object has group_id and a corresponding extent
-    return modified_groups
-
-
-# Wrapper for tilestache-clean.py
-'''
-def clear_cache(bbox, scale):
-    print "--- Cleaning cache ---"
-    cmd = ["python", "TileStache/scripts/tilestache-clean.py", "-q", "-b", bbox[0], bbox[1], bbox[2], bbox[3], "-c", "tilestache.cfg", "-l", ("lookup_" + scale)]
-    cmd.extend(scale_map[scale])
-    call(cmd)
-'''
-
-# Wrapper for tilestache-seed.py
-def seed_cache(bbox, scale):
-    check_call("rm -rf tmp/*", shell=True)
-    split_limit = {
-        "tiny": "300",
-        "small": "1000",
-        "medium": "5000",
-        "large": "20000"
-    }
-    cmd = "python TileStache/scripts/tilestache-list.py -b " + bbox + " "  +  " ".join(scale_map[scale]) + " | split -l " + split_limit[scale] + " - tmp/list- && ls -1 tmp/list-* | xargs -n1 -P" + str(cpus) + " TileStache/scripts/tilestache-seed.py -q -c tilestache.cfg -l burwell_" + scale + " --tile-list"
-
-    #print cmd
-    try:
-        check_call(cmd, shell=True)
-    except CalledProcessError:
-        print "Error seeding cache at scale", scale
-        sys.exit()
-
-
 # First update the cartocss and project file
 setup()
-
-# Process the commandline args
-if arguments.all:
-    print "Do everything"
-
-    # For each scale...
-    for scale in ["tiny", "small", "medium", "large"]:
-        # Get a list of groups and their bboxes
-        #groups = find_groups(scale)
-        print "--- Seeding cache for " + scale + " ---"
-        # Chop it up
-        print "---- 1 ----"
-        seed_cache("-89 -179 0 0", scale)
-        print "---- 2 ----"
-        seed_cache("0 -179 89 0", scale)
-        print "---- 3 ----"
-        seed_cache("-89 0 0 179", scale)
-        print "---- 4 ----"
-        seed_cache("0 0 89 179", scale)
-
-        # For each group...
-        #for idx, group in enumerate(groups):
-        #    print "--- ", (idx + 1), " of ", len(groups), " ---"
-            # Clear cache
-            #clear_cache(group["extent"], scale)
-            # Reseed cache
-        #    seed_cache(group["extent"], scale)
-
-        try:
-            check_call("cp -R TileStache/tiles/burwell_" + scale + "/* TileStache/tiles/burwell", shell=True)
-        except CalledProcessError:
-            print "Error moving tiles to burwell directory @ ", scale
-            sys.exit()
-
-    clean_up()
-
-
-
-elif arguments.source_id:
-    print "Do source_id"
-
-    # Find the scale of this source_id
-    cur.execute("SELECT scale from maps.sources WHERE source_id = %(source_id)s", {
-        "source_id": arguments.source_id
-    })
-    scale = cur.fetchone()
-    if scale is None:
-        print "Invalid source_id given"
-        sys.exit(1)
-
-    # Find the group and corresponding bbox of this source_id
-    group = find_groups(scale[0], arguments.source_id)
-
-    print group
-    group = group[0]
-
-    # Clear the cache for this group
-    clear_cache(group["extent"], scale[0])
-    # Reseed cache
-    seed_cache(group["extent"], scale[0])
-
-
-elif arguments.scale:
-    print "Do scale"
-
-    # Get a list of groups and their bboxes for this scale
-    groups = find_groups(arguments.scale)
-
-    # For each group...
-    for group in groups:
-        # Clear cache
-        clear_cache(group["extent"], arguments.scale)
-        # Reseed cache
-        seed_cache(group["extent"], arguments.scale)
-
-
-else:
-    print "Invalid params/combination of params"
-    return_help()
+clean_up()
---
+++ map-integration/tiles/seed.js
@@ -0,0 +1,213 @@
+// Depdendencies for tile server
+var tilestrata = require('tilestrata');
+var disk = require('tilestrata-disk');
+var sharp = require('tilestrata-sharp');
+var mapnik = require('tilestrata-mapnik');
+var dependency = require('tilestrata-dependency');
+
+// Depdendencies for tile seeding
+var cover = require("tile-cover");
+var async = require("async");
+var http = require("http");
+var pg = require("pg");
+var credentials = require("./credentials");
+
+var strata = tilestrata.createServer();
+
+
+// define layers
+strata.layer('burwell_tiny')
+    .route('tile@2x.png')
+        .use(disk.cache({dir: credentials.settings.cachePath}))
+        .use(mapnik({
+            xml: credentials.settings.configPath + 'burwell_tiny.xml',
+            tileSize: 512,
+            scale: 2
+        }))
+    .route('tile.png')
+        .use(disk.cache({dir: credentials.settings.cachePath}))
+        .use(dependency('burwell_tiny', 'tile@2x.png'))
+        .use(sharp(function(image, sharp) {
+            return image.resize(256);
+        }));
+
+strata.layer('burwell_small')
+    .route('tile.png')
+        .use(disk.cache({dir: credentials.settings.cachePath}))
+        .use(mapnik({
+            xml: credentials.settings.configPath + 'burwell_small.xml',
+            tileSize: 512,
+            scale: 2
+        }));
+
+strata.layer('burwell_medium')
+    .route('tile.png')
+        .use(disk.cache({dir: credentials.settings.cachePath}))
+        .use(mapnik({
+            xml: credentials.settings.configPath + 'burwell_medium.xml',
+            tileSize: 512,
+            scale: 2
+        }));
+
+strata.layer('burwell_large')
+    .route('tile.png')
+        .use(disk.cache({dir: credentials.settings.cachePath}))
+        .use(mapnik({
+            xml: credentials.settings.configPath + 'burwell_large.xml',
+            tileSize: 512,
+            scale: 2
+        }));
+
+// start accepting requests
+strata.listen(credentials.settings.port);
+
+
+function queryPg(db, sql, params, callback, send, res, format, next) {
+  pg.connect("postgres://" + credentials.pg.user + "@" + credentials.pg.host + "/" + db, function(err, client, done) {
+    if (err) {
+      callback(err);
+    } else {
+      var query = client.query(sql, params, function(err, result) {
+        done();
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, result);
+        }
+      });
+    }
+  });
+}
+
+var scaleHash = {
+  "tiny": [0, 1, 2, 3, 4],
+  "small": [5, 6],
+  "medium": [7, 8, 9, 10],
+  "large": [12, 13]
+}
+
+
+var scaleGroups = {
+  "tiny": ["tiny"],
+  "small": ["tiny", "large", "medium", "small"],
+  "medium": ["large", "small", "medium"],
+  "large": ["medium", "large"]
+}
+
+var coords = {
+  0: [],
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+  10: [],
+  11: [],
+  12: []
+}
+
+//var scales = ["tiny", "small", "medium", "large"];
+var scales = ["tiny"];
+
+async.eachLimit(scales, 1, function(scale, scaleCallback) {
+  console.log("Working on ", scale);
+
+  // These are source-specific additions to land
+  var extras;
+
+  async.series([
+    function(callback) {
+      queryPg("burwell", "SELECT ST_AsGeoJSON(geom, 4) AS geometry FROM public.land", [], function(error, data) {
+        if (error) {
+          console.log(error);
+        }
+
+        async.each(scaleHash[scale], function(z, zcallback) {
+          async.each(data.rows, function(i, icallback) {
+            var coverage = []
+
+            coverage = cover.tiles(JSON.parse(i.geometry), {min_zoom: z, max_zoom: z});
+            if (coverage.length && coverage.length < 100000) {
+              coords[z].push.apply(coords[z], coverage)
+              icallback(null);
+            } else {
+              icallback(null);
+            }
+          }, function(error) {
+            zcallback(null);
+          })
+        }, function(error) {
+          callback(null);
+        });
+      });
+    },
+
+    function(callback) {
+      queryPg("burwell", "\
+        SELECT geometry FROM ( \
+        SELECT ST_AsGeoJSON(ST_Simplify(((st_dump(geom)).geom), 1), 4) AS geometry FROM \
+        (SELECT \
+          ST_Difference( \
+            (SELECT ST_Buffer(ST_Collect(ref_geom),0) FROM maps.sources WHERE sources.source_id IN (SELECT source_id FROM maps.sources WHERE scale = ANY($1))), \
+            (SELECT ST_Buffer(ST_Collect(geom),0) FROM public.land) \
+          ) geom \
+        ) sub ) foo WHERE geometry is NOT NULL", [scaleGroups[scale]], function(error, data) {
+        if (error) {
+          console.log(error);
+        }
+        extras = data;
+        callback(null)
+      });
+    },
+
+    function(callback) {
+      async.eachLimit(scaleHash[scale], 1, function(z, zoomCallback) {
+        console.log("     z", z);
+
+        var newCoords = [];
+
+        for (var i = 0; i < extras.rows.length; i++) {
+          newCoords.push.apply(newCoords, (cover.tiles(JSON.parse(extras.rows[i].geometry), {min_zoom: z, max_zoom: z})));
+        }
+        //console.log(z, coords[z].length)
+
+        var allTiles = coords[z].concat(newCoords);
+        var foundTiles = {}
+
+        var unique = allTiles.filter(function(d) {
+          if (!foundTiles[d.join(',')]) {
+            foundTiles[d.join(',')] = true;
+            return d;
+          }
+        });
+
+        async.eachLimit(unique, 10, function(tile, tileCallback) {
+          http.get('http://localhost:' + credentials.settings.port + '/burwell_' + scale + '/' + tile[2] + '/' + tile[0] + '/' + tile[1] + '/tile.png', function(res) {
+            tileCallback(null);
+          });
+        }, function(err) {
+          if (err) {
+            console.log(err);
+          }
+          zoomCallback(null);
+        });
+      }, function(err) {
+        if (err) {
+          console.log(err);
+        }
+        callback(null);
+      });
+    }
+
+  ], function(error, result) {
+    scaleCallback(null);
+  });
+
+}, function(error, results) {
+  console.log("Done seeding")
+  process.exit();
+});
