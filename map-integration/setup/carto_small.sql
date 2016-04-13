7db0cc0d281dd971f9e48ad774304098b044c6a8
John J Czaplewski <jczaplew@gmail.com>
2016-04-13 16:30:49 -0500

    Add script for creating foc geom

---
+++ map-integration/setup/carto_small.sql
@@ -0,0 +1,121 @@
+DROP TABLE IF EXISTS carto.small;
+-- Format small
+create table carto.small as
+WITH priority_ref AS (
+  SELECT 1 AS id, ST_SetSRID(ST_Union(rgeom), 4326) geom
+  FROM maps.sources
+  WHERE scale = 'small'
+  AND priority IS TRUE
+),
+priorities AS (
+  SELECT s.map_id, s.geom
+  FROM maps.small s
+  JOIN maps.sources ON s.source_id = sources.source_id
+  WHERE priority IS TRUE
+),
+nonpriority_unique AS (
+  SELECT s.map_id, s.geom
+  FROM maps.small s
+  JOIN maps.sources ON s.source_id = sources.source_id
+  LEFT JOIN priority_ref pr
+  ON ST_Intersects(s.geom, st_setsrid(pr.geom, 4326))
+  WHERE pr.id IS NULL
+  AND priority IS FALSE
+  AND ST_Geometrytype(s.geom) != 'ST_LineString'
+),
+nonpriority_clipped AS (
+  SELECT s.map_id, ST_Difference(s.geom, pr.geom) geom
+  FROM maps.small s
+  JOIN priority_ref pr
+  ON ST_Intersects(s.geom, pr.geom)
+  JOIN maps.sources ON s.source_id = sources.source_id
+  WHERE priority IS FALSE
+),
+small AS (
+  SELECT map_id, geom
+  FROM priorities
+  WHERE ST_NumGeometries(geom) > 0
+  UNION
+  SELECT map_id, geom
+  FROM nonpriority_unique
+  WHERE ST_NumGeometries(geom) > 0
+  UNION
+  SELECT map_id, geom
+  FROM nonpriority_clipped
+  WHERE ST_NumGeometries(geom) > 0
+),
+-- Format tiny
+tiny_priority_ref AS (
+  SELECT 1 AS id, ST_SetSRID(ST_Union(rgeom), 4326) geom
+  FROM maps.sources
+  WHERE scale = 'tiny'
+  AND priority IS TRUE
+),
+tiny_priorities AS (
+  SELECT s.map_id, s.geom
+  FROM maps.tiny s
+  JOIN maps.sources ON s.source_id = sources.source_id
+  WHERE priority IS TRUE
+),
+tiny_nonpriority_unique AS (
+  SELECT s.map_id, s.geom
+  FROM maps.tiny s
+  JOIN maps.sources ON s.source_id = sources.source_id
+  LEFT JOIN tiny_priority_ref pr
+  ON ST_Intersects(s.geom, st_setsrid(pr.geom, 4326))
+  WHERE pr.id IS NULL
+  AND priority IS FALSE
+  AND ST_Geometrytype(s.geom) != 'ST_LineString'
+),
+tiny_nonpriority_clipped AS (
+  SELECT s.map_id, ST_Difference(s.geom, pr.geom) geom
+  FROM maps.tiny s
+  JOIN tiny_priority_ref pr
+  ON ST_Intersects(s.geom, pr.geom)
+  JOIN maps.sources ON s.source_id = sources.source_id
+  WHERE priority IS FALSE
+),
+tiny AS (
+  SELECT map_id, geom
+  FROM tiny_priorities
+  WHERE ST_NumGeometries(geom) > 0
+  UNION
+  SELECT map_id, geom
+  FROM tiny_nonpriority_unique
+  WHERE ST_NumGeometries(geom) > 0
+  UNION
+  SELECT map_id, geom
+  FROM tiny_nonpriority_clipped
+  WHERE ST_NumGeometries(geom) > 0
+),
+small_ref AS (
+  SELECT 1 AS id, ST_SetSRID(ST_Union(rgeom), 4326) geom
+  FROM maps.sources
+  WHERE scale = 'small'
+),
+unique_tiny AS (
+  SELECT t.map_id, t.geom
+  FROM tiny t
+  LEFT JOIN small_ref sr
+  ON ST_Intersects(t.geom, sr.geom)
+  WHERE sr.id IS NULL
+  AND ST_Geometrytype(t.geom) != 'ST_LineString'
+),
+tiny_clipped AS (
+  SELECT t.map_id, ST_Difference(t.geom, sr.geom) geom
+  FROM tiny t
+  JOIN small_ref sr
+  ON ST_Intersects(t.geom, sr.geom)
+)
+
+SELECT map_id, geom
+FROM small
+UNION
+SELECT map_id, geom
+FROM unique_tiny
+UNION
+SELECT map_id, geom
+FROM tiny_clipped;
+
+CREATE INDEX ON carto.small (map_id);
+CREATE INDEX ON carto.small USING GiST (geom);
