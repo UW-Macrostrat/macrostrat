26b0150437b5949e33e9d2c5a7ee0dc5734f6a1d
John J Czaplewski <jczaplew@gmail.com>
2016-05-05 18:12:25 -0500

    Add script for building carto tables

---
+++ map-integration/setup/build_carto.js
@@ -0,0 +1,192 @@
+var config = require('../tiles/config');
+var credentials = require('../tiles/credentials');
+var pg = require('pg');
+
+if (!process.argv[2] || config.scales.indexOf(process.argv[2]) < 0) {
+  console.log('Please provide a valid burwell scale');
+  process.exit(1)
+}
+
+function queryPg(sql, params, callback) {
+  pg.connect('postgres://' + credentials.pg.user + '@' + credentials.pg.host + '/burwell', function(err, client, done) {
+    if (err) {
+      callback(err);
+    } else {
+      var query = client.query(sql, params, function(err, result) {
+        done();
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, result);
+        }
+      });
+    }
+  });
+}
+
+function prepScale(scale) {
+  return `
+  ${scale}_priority_ref AS (
+    SELECT 1 AS id, ST_SetSRID(ST_Union(rgeom), 4326) geom
+    FROM maps.sources
+    WHERE scale = '${scale}'
+    AND priority IS TRUE
+  ),
+  ${scale}_priorities AS (
+    SELECT s.map_id, s.geom
+    FROM maps.${scale} s
+    JOIN maps.sources ON s.source_id = sources.source_id
+    WHERE priority IS TRUE
+  ),
+  ${scale}_nonpriority_unique AS (
+    SELECT s.map_id, s.geom
+    FROM maps.${scale} s
+    JOIN maps.sources ON s.source_id = sources.source_id
+    LEFT JOIN ${scale}_priority_ref pr
+    ON ST_Intersects(s.geom, st_setsrid(pr.geom, 4326))
+    WHERE pr.id IS NULL
+    AND priority IS FALSE
+    AND ST_Geometrytype(s.geom) != 'ST_LineString'
+  ),
+  ${scale}_nonpriority_clipped AS (
+    SELECT s.map_id, ST_Difference(s.geom, pr.geom) geom
+    FROM maps.${scale} s
+    JOIN ${scale}_priority_ref pr
+    ON ST_Intersects(s.geom, pr.geom)
+    JOIN maps.sources ON s.source_id = sources.source_id
+    WHERE priority IS FALSE
+  ),
+  ${scale} AS (
+    SELECT map_id, geom
+    FROM ${scale}_priorities
+    WHERE ST_NumGeometries(geom) > 0
+    UNION
+    SELECT map_id, geom
+    FROM ${scale}_nonpriority_unique
+    WHERE ST_NumGeometries(geom) > 0
+    UNION
+    SELECT map_id, geom
+    FROM ${scale}_nonpriority_clipped
+    WHERE ST_NumGeometries(geom) > 0
+  )`;
+}
+
+var sql = `
+DROP TABLE IF EXISTS carto.${process.argv[2]};
+CREATE TABLE carto.${process.argv[2]} AS WITH `;
+
+var scalePrepSQL = [];
+
+//var scales = ['tiny', 'small'];
+ var scales = config.layerOrder[process.argv[2]];
+
+scales.forEach(function(scale) {
+  scalePrepSQL.push(prepScale(scale));
+});
+
+// Unique bottom and clipped bottom
+scales.forEach(function(scale, idx) {
+  if (idx === scales.length - 1) {
+    return;
+  }
+  var scalesAbove = scales.map(function(d, i) {
+    if (i > idx) return "'" + d + "'";
+  }).filter(function(d) {
+    if (d) return d;
+  }).join(',');
+
+  scalePrepSQL.push(`
+  unique_${scale} AS (
+    SELECT t.map_id, t.geom
+    FROM ${scale} t
+    LEFT JOIN (
+      SELECT 1 AS id, ST_SetSRID(ST_Union(rgeom), 4326) geom
+      FROM maps.sources
+      WHERE scale IN (${scalesAbove})
+    ) sr
+    ON ST_Intersects(t.geom, sr.geom)
+    WHERE sr.id IS NULL
+    AND ST_Geometrytype(t.geom) != 'ST_LineString'
+  ),
+  ${scale}_clipped AS (
+    SELECT t.map_id, ST_Difference(t.geom, sr.geom) geom
+    FROM ${scale} t
+    JOIN (
+      SELECT 1 AS id, ST_SetSRID(ST_Union(rgeom), 4326) geom
+      FROM maps.sources
+      WHERE scale IN (${scalesAbove})
+    ) sr
+    ON ST_Intersects(t.geom, sr.geom)
+  )`);
+});
+
+var resultQuery = 'result AS ( '
+
+// Union everything
+scales.forEach(function(scale, idx) {
+  if (idx != scales.length - 1) {
+    resultQuery += `
+    SELECT map_id, '${scale}' AS scale, geom
+    FROM unique_${scale}
+    UNION
+    SELECT map_id, '${scale}' AS scale, geom
+    FROM ${scale}_clipped
+    UNION`
+  } else {
+    resultQuery += `
+    SELECT map_id, '${scale}' AS scale, geom
+    FROM ${scale}
+    `
+  }
+});
+
+resultQuery += ')';
+scalePrepSQL.push(resultQuery);
+sql += scalePrepSQL.join(',');
+
+var mJoin = scales.map(function(scale) {
+  return `SELECT map_id, source_id, name, strat_name, age, lith, descrip, comments, t_interval, b_interval FROM maps.${scale}`;
+}).join(' UNION ');
+var lJoin = scales.map(function(scale) {
+  return `SELECT map_id, best_age_top, best_age_bottom, color FROM public.lookup_${scale}`
+}).join(' UNION ');
+
+sql += `
+SELECT r.map_id, r.scale, m.source_id,
+COALESCE(m.name, '') AS name,
+COALESCE(m.strat_name, '') AS strat_name,
+COALESCE(m.age, '') AS age,
+COALESCE(m.lith, '') AS lith,
+COALESCE(m.descrip, '') AS descrip,
+COALESCE(m.comments, '') AS comments,
+cast(l.best_age_top as numeric) AS best_age_top,
+cast(l.best_age_bottom as numeric) AS best_age_bottom, it.interval_name t_int, ib.interval_name b_int, l.color,
+ST_SetSRID(r.geom, 4326) AS geom
+FROM result r
+LEFT JOIN (
+  ${mJoin}
+) m ON r.map_id = m.map_id
+LEFT JOIN (
+  ${lJoin}
+) l ON r.map_id = l.map_id
+JOIN macrostrat.intervals it ON m.t_interval = it.id
+JOIN macrostrat.intervals ib ON m.b_interval = ib.id
+WHERE ST_NumGeometries(r.geom) > 0;
+
+CREATE INDEX ON carto.${process.argv[2]} (map_id);
+CREATE INDEX ON carto.${process.argv[2]} USING GiST (geom);
+
+`;
+
+console.time('query');
+queryPg(sql, [], function(error) {
+  if (error) {
+    console.log(sql);
+    console.log('Something went wrong ', error);
+    process.exit(1);
+  } else {
+    console.log('Created table carto.' + process.argv[2])
+    console.timeEnd('query');
+    process.exit(2);
+  }
+});
