cba9c4aff1c0a5960e1174e1ca6cdb84f16c7092
John J Czaplewski <jczaplew@gmail.com>
2016-10-05 14:20:17 -0500

    Add missing shell call

--- map-integration/utils/carto.py
+++ map-integration/utils/carto.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
         call(['mapshaper -i rgeoms.shp -dissolve -o %s_rgeom.shp' % (scale, )], shell=True)
 
         # Import it
-        call(['shp2pgsql -s 4326 %s_rgeom.shp public.%s_rgeom | psql -h %s -p %s -U %s -d burwell' % (scale, scale, credentials.pg_host, credentials.pg_port, credentials.pg_user)])
+        call(['shp2pgsql -s 4326 %s_rgeom.shp public.%s_rgeom | psql -h %s -p %s -U %s -d burwell' % (scale, scale, credentials.pg_host, credentials.pg_port, credentials.pg_user)], shell=True)
 
         # Export intersecting geom
         call(['pgsql2shp -f intersecting.shp -u %s, -h %s -p %s burwell "SELECT t.map_id, t.geom FROM carto.flat_%s t JOIN public.%s_rgeom sr ON ST_Intersects(t.geom, sr.geom)"' % (credentials.pg_user, credentials.pg_host, credentials.pg_port, scale, scale)], shell=True )
