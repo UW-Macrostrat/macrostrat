014b6d64bb5d4435e1646404dde6fd53a9d5707c
John J Czaplewski <jczaplew@gmail.com>
2016-10-13 15:10:16 -0500

    add one stop shop for source processing

---
+++ map-integration/process_source.py
@@ -0,0 +1,73 @@
+import argparse
+import psycopg2
+import psycopg2.extras
+from psycopg2.extensions import AsIs
+from subprocess import call
+import sys
+import os
+import datetime
+
+sys.path = [os.path.join(os.path.dirname(__file__), os.pardir)] + sys.path
+import credentials
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+    description="Process a burwell source. Matches to macrostrat units, refreshes lookup tables, and populates rgeom",
+    epilog="Example usage: python match.py --source_id 2")
+
+    parser.add_argument("-s", "--source_id", dest="source_id",
+    default="0", type=str, required=True,
+    help="The ID of the desired source to match")
+
+    arguments = parser.parse_args()
+
+    connection = psycopg2.connect(dbname="burwell", user=credentials.pg_user, host=credentials.pg_host, port=credentials.pg_port)
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    # Validate params!
+    # Valid source_id
+    cursor.execute("SELECT source_id FROM maps.sources")
+    sources = cursor.fetchall()
+    source_ids = [source['source_id'] for source in sources]
+    if int(arguments.source_id) not in source_ids:
+        print "Invalid source_id argument. Source ID ", arguments.source_id, " was not found in maps.sources"
+        sys.exit(1)
+
+    # Find scale table
+    scale = ""
+    for scale_table in ["tiny", "small", "medium", "large"]:
+        cursor  .execute("SELECT * FROM maps.%(table)s WHERE source_id = %(source_id)s LIMIT 1", {
+          "table": AsIs(scale_table),
+          "source_id": arguments.source_id
+        })
+        if cursor.fetchone() is not None:
+          scale = scale_table
+          break
+
+    if len(scale) < 1:
+        print "Provided source_id not found in maps.small, maps.medium, or maps.large. Please insert it and try again."
+        sys.exit(1)
+
+    print 'Starting at ', str(datetime.datetime.now())
+
+    # Match
+    print '| | | | | Matching...'
+    call(['python matching/match_parallel.py --source_id %s' % (arguments.source_id, )], shell=True)
+    print '| | | | | Done matching | | | | |'
+
+    # Refresh lookup tables
+    print '| | | | | Refreshing lookup tables...'
+    call(['python setup/refresh_lookup.py %s' % (arguments.source_id, ) ], shell=True)
+    print '| | | | | Done refreshing lookup tables | | | | |'
+
+    # Build rgeom
+    print '| | | | | Building rgeom...'
+    call(['python import_scripts/rgeom.py %s' % (arguments.source_id, ) ], shell=True)
+    print '| | | | | Done building rgeom | | | | |'
+
+    # Refresh carto lines
+    print '| | | | | Adding to carto.lines...'
+    call(['python utils/carto_lines.py --source_id %s' % (arguments.source_id, )], shell=True)
+    print '| | | | | Done adding to carto.lines | | | | | '
+    
+    # Make tiles
