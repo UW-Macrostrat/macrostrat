66e4c6662403d1e72187ed146cfda8be7a484ec5
John J Czaplewski <jczaplew@gmail.com>
2016-10-24 20:25:40 -0500

    remove lookup refresh from match_parallel

--- map-integration/matching/match_parallel.py
+++ map-integration/matching/match_parallel.py
@@ -141,9 +141,3 @@ if __name__ == '__main__':
     tasks.put(None)
 
   tasks.join()
-
-  scale = refresh.find_scale(cursor, arguments.source_id)
-  if scale is not None:
-      refresh.refresh(cursor, connection, scale, arguments.source_id)
-      refresh.source_stats(cursor, connection, arguments.source_id)
-      print "        Refreshed lookup_" + scale + " for source_id ", arguments.source_id
