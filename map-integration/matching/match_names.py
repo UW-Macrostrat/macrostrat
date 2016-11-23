864de8f9f93b2af05cfc470ce7e06f5339731ed1
John J Czaplewski <john@czaplewski.org>
2016-11-23 16:18:14 -0600

    Add name matching scripts

---
+++ map-integration/matching/match_names.py
@@ -0,0 +1,136 @@
+import multiprocessing
+from match_names_multi import *
+import argparse
+import psycopg2
+import psycopg2.extras
+from psycopg2.extensions import AsIs
+import sys
+import os
+import datetime
+
+sys.path = [os.path.join(os.path.dirname(__file__), os.pardir)] + sys.path
+import credentials
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+split_path = cwd.split("/")
+split_path.pop()
+
+sys.path.insert(0, "/".join(split_path) + "/setup")
+
+import refresh
+
+if __name__ == '__main__':
+
+  connection = psycopg2.connect(dbname="burwell", user=credentials.pg_user, host=credentials.pg_host, port=credentials.pg_port)
+  cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+  parser = argparse.ArgumentParser(
+    description="Match rocks to Macrostrat units",
+    epilog="Example usage: python match_names.py --source_id 2")
+
+  parser.add_argument("-s", "--source_id", dest="source_id",
+    default="0", type=str, required=True,
+    help="The ID of the desired source to match")
+
+  parser.add_argument("-e", "--exclude", dest="exclude",
+    default="", type=str, required=False,
+    help="Field(s) that should be ommitted from the matching process. Ex: --exclude descrip,comments")
+
+  arguments = parser.parse_args()
+
+  # Validate params!
+  # Valid source_id
+  cursor.execute("SELECT source_id FROM maps.sources")
+  sources = cursor.fetchall()
+  source_ids = [source['source_id'] for source in sources]
+  if int(arguments.source_id) not in source_ids:
+      print "Invalid source_id argument. Source ID ", arguments.source_id, " was not found in maps.sources"
+      sys.exit(1)
+
+  # Find scale table
+  scale = ""
+  for scale_table in ["tiny", "small", "medium", "large"]:
+      cursor.execute("SELECT * FROM maps.%(table)s WHERE source_id = %(source_id)s LIMIT 1", {
+        "table": AsIs(scale_table),
+        "source_id": arguments.source_id
+      })
+      if cursor.fetchone() is not None:
+        scale = scale_table
+        break
+
+  if len(scale) < 1:
+      print "Provided source_id not found in maps.small, maps.medium, or maps.large. Please insert it and try again."
+      sys.exit(1)
+
+
+  print 'Starting at ', str(datetime.datetime.now())
+
+
+  # Clean up
+  cursor.execute("""
+      DELETE FROM maps.map_strat_names WHERE map_id IN (SELECT map_id FROM maps.%(table)s WHERE source_id = %(source_id)s) AND basis_col NOT LIKE 'manual%%'
+  """, {
+      "table": AsIs(scale),
+      "source_id": arguments.source_id
+  })
+  connection.commit()
+  print "        + Done cleaning up"
+
+
+
+  tasks = multiprocessing.JoinableQueue()
+  results = multiprocessing.Queue()
+
+  num_processors = 1
+  if multiprocessing.cpu_count() < 4 :
+      num_processors = multiprocessing.cpu_count()
+  else :
+      num_processors = 4
+
+  processors = [Processor(tasks, results) for i in xrange(num_processors)]
+
+  for each in processors:
+    each.start()
+
+  ### Define our tasks ###
+
+  # Fields in burwell to match on
+  fields = ["strat_name", "name", "descrip", "comments"]
+
+  # Remove the fields explicitly excluded
+  exclude = arguments.exclude.split(",")
+  if len(exclude) > 0:
+      for field in exclude:
+        try :
+          fields.remove(field)
+        except:
+          print '        + Not excluding invalid field selection ', field
+
+  # Filter null fields
+  cursor.execute("""
+    SELECT
+        count(distinct strat_name)::int AS strat_name,
+        count(distinct name)::int AS name,
+        count(distinct descrip)::int AS descrip,
+        count(distinct comments)::int AS comments
+    FROM maps.%(scale)s where source_id = %(source_id)s;
+  """, {
+    "scale": AsIs(scale),
+    "source_id": arguments.source_id
+  })
+  result = cursor.fetchone()
+
+  for field in result:
+      if result[field] == 0:
+          fields.remove(field)
+          print '        + Excluding', field, 'because it is null'
+
+  # Insert a new task for each matching field into the queue
+  for field in fields:
+      tasks.put(Task(scale, arguments.source_id, field))
+
+  for i in range(num_processors):
+    tasks.put(None)
+
+  tasks.join()
---
+++ map-integration/matching/match_names_multi.py
@@ -0,0 +1,150 @@
+# Adapted/borrowed from http://stackoverflow.com/a/7556042/1956065
+import multiprocessing
+import psycopg2
+from psycopg2.extensions import AsIs
+import time
+import sys
+import os
+
+sys.path = [os.path.join(os.path.dirname(__file__), os.pardir)] + sys.path
+import credentials
+
+
+class Processor(multiprocessing.Process):
+
+  def __init__(self, task_queue, result_queue):
+    multiprocessing.Process.__init__(self)
+    self.task_queue = task_queue
+    self.result_queue = result_queue
+    self.pyConn = psycopg2.connect(dbname=credentials.pg_db, user=credentials.pg_user, host=credentials.pg_host, port=credentials.pg_port)
+    self.pyConn.set_isolation_level(0)
+
+
+  def run(self):
+    proc_name = self.name
+    while True:
+      next_task = self.task_queue.get()
+      if next_task is None:
+          #print 'Tasks complete on this thread'
+          self.task_queue.task_done()
+          break
+      answer = next_task(connection=self.pyConn)
+      self.task_queue.task_done()
+      self.result_queue.put(answer)
+    return
+
+
+class Task(object):
+  # Assign check and year when initialized
+  def __init__(self, table, source_id, field):
+    self.table = table
+    self.source_id = source_id
+    self.field = field
+
+  # Acts as the controller for a given year
+  def __call__(self, connection=None):
+
+    def query(strictNameMatch, strictSpace, strictTime, useNullSet):
+        match_type = field
+
+        if not strictNameMatch:
+            match_type += '_fname'
+
+        if not strictSpace:
+            match_type += '_fspace'
+
+        if not strictTime:
+            match_type += '_ftime'
+
+        pyCursor.execute("""
+        INSERT INTO maps.map_strat_names
+            WITH cull AS (
+                SELECT *
+                FROM macrostrat.strat_name_footprints snft
+                WHERE snft.strat_name_id IN (
+                    WITH macro_flat AS (
+                      SELECT strat_name_id, unnest(string_to_array(""" + ("replace(lsn3.rank_name, '.', '')" if strictNameMatch else "replace(lsn3.name_no_lith, '.', '')") + """, ' ')) as token
+                      FROM macrostrat.lookup_strat_names lsn3
+                    ),
+                    names AS (
+                        SELECT DISTINCT strat_name_id
+                        FROM macro_flat
+                        WHERE token IN (
+                          SELECT DISTINCT unnest(string_to_array(name, ' '))
+                          FROM maps.%(table)s
+                          WHERE source_id = %(source_id)s
+                        )
+                    )
+                    SELECT DISTINCT lsn4.strat_name_id
+                    FROM macrostrat.lookup_strat_names AS lsn4
+                    CROSS JOIN (
+                      SELECT DISTINCT replace(%(field)s, '.', '') AS rock_match_field
+                      FROM maps.%(table)s
+                      WHERE source_id = %(source_id)s
+                    ) AS distinct_rocks
+                    JOIN names ON lsn4.strat_name_id = names.strat_name_id
+                    WHERE distinct_rocks.rock_match_field ~* concat('\y', """ + ("replace(lsn4.rank_name, '.', '')" if strictNameMatch else "replace(lsn4.name_no_lith, '.', '')") + """, '\y')
+                )
+            )
+            SELECT rocks.map_id, strat_name_id, %(match_type)s
+            FROM cull snft
+            JOIN maps.%(table)s rocks ON ST_Intersects(""" + ("snft.geom" if strictSpace else "ST_Buffer(snft.geom, 1.2)") + """, rocks.geom)
+            JOIN macrostrat.intervals intervals_top on rocks.t_interval = intervals_top.id
+            JOIN macrostrat.intervals intervals_bottom on rocks.b_interval = intervals_bottom.id
+            WHERE rocks.source_id = %(source_id)s
+            AND ((snft.best_t_age) < (intervals_bottom.age_bottom + """ + ("0" if strictTime else "25") + """))
+            AND ((snft.best_b_age) > (intervals_top.age_top - """ + ("0" if strictTime else "25") + """))
+            AND rocks.%(field)s ~* concat('\y', """ + ("replace(snft.rank_name, '.', '')" if strictNameMatch else "replace(snft.name_no_lith, '.', '')") + """, '\y')
+        """, {
+          "table": AsIs(table),
+          "source_id": source_id,
+          "field": AsIs(field),
+          "match_type": match_type
+        })
+
+        pyConn.commit()
+
+        print "        - Done with " + match_type
+
+
+    pyConn = connection
+    pyCursor = pyConn.cursor()
+
+    # Time the process
+    start_time = time.time()
+
+    print "      * Working on ", self.field, " *"
+
+    field = self.field
+    table = self.table
+    source_id = self.source_id
+
+    # strictNameMatch, strictSpace, strictTime, useNullSet
+
+    # Strict name, strict space, strict time
+    a = query(True, True, True, False)
+
+    # Strict name, fuzzy space, strict time
+    b = query(True, False, True, True)
+
+    # Fuzzy name, strict space, strict time
+    c = query(False, True, True, True)
+
+    # Strict name, strict space, fuzzy time
+    d = query(True, True, False, True)
+
+    # Fuzzy name, fuzzy space, strict time
+    e = query(False, False, True, True)
+
+    # Strict name, fuzzy space, fuzzy time
+    f = query(True, False, False, True)
+
+    # Fuzzy name, strict space, fuzzy time
+    g = query(False, True, False, True)
+
+    # Fuzzy name, fuzzy space, fuzzy time
+    h = query(False, False, False, True)
+
+
+    elapsed = int(time.time() - start_time)
+    print "        Done with ", self.field, " in ", elapsed / 60, " minutes and ", elapsed % 60, " seconds"
