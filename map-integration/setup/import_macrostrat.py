6cf57a5003fddeed3007c7595788509f7d384eec
John J Czaplewski <john@czaplewski.org>
2017-05-11 14:32:51 -0500

    Fix strat name footprint geometries

--- map-integration/setup/import_macrostrat.py
+++ map-integration/setup/import_macrostrat.py
@@ -723,6 +723,10 @@ LEFT JOIN macrostrat_new.intervals tb ON strat_names_meta.b_int = tb.id
 UPDATE macrostrat_new.strat_name_footprints SET geom = 'POLYGON EMPTY' WHERE ST_GeometryType(geom) = 'ST_Point';
 UPDATE macrostrat_new.strat_name_footprints SET geom = ST_SetSRID(geom, 4326);
 
+UPDATE macrostrat.strat_name_footprints
+SET geom = st_collectionextract(geom, 3)
+WHERE st_geometrytype(geom) = 'ST_GeometryCollection';
+
 CREATE INDEX ON macrostrat_new.strat_name_footprints (strat_name_id);
 CREATE INDEX ON macrostrat_new.strat_name_footprints USING GiST (geom);
 
