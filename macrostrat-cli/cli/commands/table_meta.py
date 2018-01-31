06f038c8f8534ca33a02ae25cf19cffb68a7cbf6
John J Czaplewski <john@czaplewski.org>
2018-01-30 21:46:11 -0600

    Add missing columns

--- macrostrat-cli/cli/commands/table_meta.py
+++ macrostrat-cli/cli/commands/table_meta.py
@@ -378,7 +378,7 @@ tables = OrderedDict({
             );
         """,
         "insert": """
-            INSERT INTO macrostrat.liths_new ( id, lith, lith_group, lith_type, lith_class, lith_equiv, lith_fill, comp_coef, initial_porosity, bulk_density, lith_color) VALUES (%(id)s, %(lith)s, %(lith_type)s, %(lith_class)s, %(lith_fill)s, %(comp_coef)s, %(initial_porosity)s, %(bulk_density)s, %(lith_color)s)
+            INSERT INTO macrostrat.liths_new ( id, lith, lith_group, lith_type, lith_class, lith_equiv, lith_fill, comp_coef, initial_porosity, bulk_density, lith_color) VALUES (%(id)s, %(lith)s, %(lith_group)s, %(lith_type)s, %(lith_class)s, %(lith_equiv)s, %(lith_fill)s, %(comp_coef)s, %(initial_porosity)s, %(bulk_density)s, %(lith_color)s)
         """,
         "index": """
             CREATE INDEX ON macrostrat.liths_new (lith);
