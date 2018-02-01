d296ea204ddb8fd3712a65e8a222edc4f37b3839
Shanan Peters <cambro@gmail.com>
2018-02-01 11:40:44 -0600

    autocomplete has dependencies on build tables

--- macrostrat-cli/cli/commands/rebuild.py
+++ macrostrat-cli/cli/commands/rebuild.py
@@ -49,7 +49,7 @@ class Rebuild(Base):
 
         if table == 'all':
             # Rebuild in a specific order
-            tables = [ 'autocomplete', 'lookup_strat_names', 'lookup_unit_attrs_api', 'lookup_unit_intervals', 'pbdb_matches', 'unit_boundaries', 'lookup_units', 'stats']
+            tables = ['lookup_strat_names', 'lookup_unit_attrs_api', 'lookup_unit_intervals', 'pbdb_matches', 'unit_boundaries', 'lookup_units', 'stats', 'autocomplete']
             for t in tables:
                 print '     %s' % (t, )
                 script = getattr(rebuild_scripts, t)
