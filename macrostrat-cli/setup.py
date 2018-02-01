f60697e8b318851f3b9cb723faf9828aeaf02cb1
John J Czaplewski <john@czaplewski.org>
2018-02-01 11:24:12 -0600

    Clean up install

--- macrostrat-cli/setup.py
+++ macrostrat-cli/setup.py
@@ -50,7 +50,7 @@ setup(
     ],
     keywords = 'cli',
     packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['psycopg2', 'pymysql', 'pyyaml', 'tiletanic', 'shapely', 'fiona'],
+    install_requires = ['psycopg2', 'pymysql', 'pyyaml', 'tiletanic', 'shapely', 'fiona', 'pyproj', 'scipy'],
     # extras_require = {
     #     'test': ['coverage', 'pytest', 'pytest-cov'],
     # },
