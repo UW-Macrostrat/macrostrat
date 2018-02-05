f3207c96bac26d944ffd7c557eed9704a85e41be
John J Czaplewski <john@czaplewski.org>
2018-02-05 08:41:19 -0600

    Fix typo

--- macrostrat-cli/cli/commands/rebuild_scripts/strat_name_footprints.py
+++ macrostrat-cli/cli/commands/rebuild_scripts/strat_name_footprints.py
@@ -15,14 +15,14 @@ class StratNameFootprints(Base):
                 lsn.concept_id,
                 array(
                   SELECT DISTINCT strat_name_id
-                  FROM macrostrat_new.lookup_strat_names
+                  FROM macrostrat.lookup_strat_names
                   WHERE lookup_strat_names.bed_id = lsn.strat_name_id
                     OR lookup_strat_names.mbr_id = lsn.strat_name_id
                     OR lookup_strat_names.fm_id = lsn.strat_name_id
                     OR lookup_strat_names.gp_id = lsn.strat_name_id
                     OR lookup_strat_names.sgp_id = lsn.strat_name_id
                 ) AS names_in_hierarchy
-              FROM macrostrat_new.lookup_strat_names lsn
+              FROM macrostrat.lookup_strat_names lsn
               GROUP BY lsn.strat_name_id, lsn.concept_id, lsn.name_no_lith, lsn.rank_name
             ),
             second AS (
@@ -36,37 +36,37 @@ class StratNameFootprints(Base):
             )
             SELECT strat_name_id, name_no_lith, rank_name, second.concept_id, second.concept_names, ST_Union((
               SELECT COALESCE(ST_Union(poly_geom), 'POLYGON EMPTY') AS geom
-              FROM macrostrat_new.unit_strat_names
-              JOIN macrostrat_new.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
-              JOIN macrostrat_new.cols ON units_sections.col_id = cols.id
+              FROM macrostrat.unit_strat_names
+              JOIN macrostrat.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
+              JOIN macrostrat.cols ON units_sections.col_id = cols.id
               WHERE unit_strat_names.strat_name_id = ANY(second.concept_names)
             ), (
               SELECT COALESCE(ST_Union(geom), 'POLYGON EMPTY') as geom
-              FROM macrostrat_new.strat_names_places
-              JOIN macrostrat_new.places ON places.place_id = strat_names_places.place_id
+              FROM macrostrat.strat_names_places
+              JOIN macrostrat.places ON places.place_id = strat_names_places.place_id
               WHERE strat_names_places.strat_name_id = ANY(second.concept_names)
             )) AS geom,
             LEAST((
               SELECT min(t_age)
-              FROM macrostrat_new.lookup_unit_intervals
-              JOIN macrostrat_new.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
-              JOIN macrostrat_new.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
-              JOIN macrostrat_new.cols ON units_sections.col_id = cols.id
+              FROM macrostrat.lookup_unit_intervals
+              JOIN macrostrat.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
+              JOIN macrostrat.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
+              JOIN macrostrat.cols ON units_sections.col_id = cols.id
               WHERE unit_strat_names.strat_name_id = ANY(second.concept_names)
             ),ti.age_top) AS best_t_age,
             GREATEST((
               SELECT max(b_age)
-              FROM macrostrat_new.lookup_unit_intervals
-              JOIN macrostrat_new.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
-              JOIN macrostrat_new.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
-              JOIN macrostrat_new.cols ON units_sections.col_id = cols.id
+              FROM macrostrat.lookup_unit_intervals
+              JOIN macrostrat.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
+              JOIN macrostrat.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
+              JOIN macrostrat.cols ON units_sections.col_id = cols.id
               WHERE unit_strat_names.strat_name_id = ANY(second.concept_names)
             ), tb.age_bottom) AS best_b_age
             FROM first
             LEFT JOIN second ON first.concept_id = second.concept_id
-            LEFT JOIN macrostrat_new.strat_names_meta ON second.concept_id = strat_names_meta.concept_id
-            LEFT JOIN macrostrat_new.intervals ti ON strat_names_meta.t_int = ti.id
-            LEFT JOIN macrostrat_new.intervals tb ON strat_names_meta.b_int = tb.id
+            LEFT JOIN macrostrat.strat_names_meta ON second.concept_id = strat_names_meta.concept_id
+            LEFT JOIN macrostrat.intervals ti ON strat_names_meta.t_int = ti.id
+            LEFT JOIN macrostrat.intervals tb ON strat_names_meta.b_int = tb.id
             ;
 
             UPDATE macrostrat.strat_name_footprints_new SET geom = 'POLYGON EMPTY' WHERE ST_GeometryType(geom) = 'ST_Point';
@@ -80,14 +80,14 @@ class StratNameFootprints(Base):
                 lsn.concept_id,
                 array(
                   SELECT DISTINCT strat_name_id
-                  FROM macrostrat_new.lookup_strat_names
+                  FROM macrostrat.lookup_strat_names
                   WHERE lookup_strat_names.bed_id = lsn.strat_name_id
                     OR lookup_strat_names.mbr_id = lsn.strat_name_id
                     OR lookup_strat_names.fm_id = lsn.strat_name_id
                     OR lookup_strat_names.gp_id = lsn.strat_name_id
                     OR lookup_strat_names.sgp_id = lsn.strat_name_id
                 ) AS names_in_hierarchy
-              FROM macrostrat_new.lookup_strat_names lsn
+              FROM macrostrat.lookup_strat_names lsn
               WHERE lsn.strat_name_id NOT IN (
                 SELECT DISTINCT strat_name_id
                 FROM macrostrat.strat_name_footprints_new
@@ -103,37 +103,37 @@ class StratNameFootprints(Base):
             )
             SELECT third.strat_name_id, name_no_lith, rank_name, third.concept_id, third.concept_names, ST_Union((
               SELECT COALESCE(ST_Union(poly_geom), 'POLYGON EMPTY') AS geom
-              FROM macrostrat_new.unit_strat_names
-              JOIN macrostrat_new.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
-              JOIN macrostrat_new.cols ON units_sections.col_id = cols.id
+              FROM macrostrat.unit_strat_names
+              JOIN macrostrat.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
+              JOIN macrostrat.cols ON units_sections.col_id = cols.id
               WHERE unit_strat_names.strat_name_id = ANY(third.concept_names)
             ), (
               SELECT COALESCE(ST_Union(geom), 'POLYGON EMPTY') as geom
-              FROM macrostrat_new.strat_names_places
-              JOIN macrostrat_new.places ON places.place_id = strat_names_places.place_id
+              FROM macrostrat.strat_names_places
+              JOIN macrostrat.places ON places.place_id = strat_names_places.place_id
               WHERE strat_names_places.strat_name_id = ANY(third.concept_names)
             )) AS geom,
             LEAST((
               SELECT min(t_age)
-              FROM macrostrat_new.lookup_unit_intervals
-              JOIN macrostrat_new.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
-              JOIN macrostrat_new.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
-              JOIN macrostrat_new.cols ON units_sections.col_id = cols.id
+              FROM macrostrat.lookup_unit_intervals
+              JOIN macrostrat.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
+              JOIN macrostrat.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
+              JOIN macrostrat.cols ON units_sections.col_id = cols.id
               WHERE unit_strat_names.strat_name_id = ANY(third.concept_names)
             ),ti.age_top) AS best_t_age,
             GREATEST((
               SELECT max(b_age)
-              FROM macrostrat_new.lookup_unit_intervals
-              JOIN macrostrat_new.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
-              JOIN macrostrat_new.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
-              JOIN macrostrat_new.cols ON units_sections.col_id = cols.id
+              FROM macrostrat.lookup_unit_intervals
+              JOIN macrostrat.unit_strat_names ON unit_strat_names.unit_id = lookup_unit_intervals.unit_id
+              JOIN macrostrat.units_sections ON unit_strat_names.unit_id = units_sections.unit_id
+              JOIN macrostrat.cols ON units_sections.col_id = cols.id
               WHERE unit_strat_names.strat_name_id = ANY(third.concept_names)
             ), tb.age_bottom) AS best_b_age
             FROM third
-            LEFT JOIN macrostrat_new.strat_names_meta ON third.concept_id = strat_names_meta.concept_id
-            JOIN macrostrat_new.lookup_strat_names lsn ON lsn.strat_name_id = third.strat_name_id
-            LEFT JOIN macrostrat_new.intervals ti ON strat_names_meta.t_int = ti.id
-            LEFT JOIN macrostrat_new.intervals tb ON strat_names_meta.b_int = tb.id
+            LEFT JOIN macrostrat.strat_names_meta ON third.concept_id = strat_names_meta.concept_id
+            JOIN macrostrat.lookup_strat_names lsn ON lsn.strat_name_id = third.strat_name_id
+            LEFT JOIN macrostrat.intervals ti ON strat_names_meta.t_int = ti.id
+            LEFT JOIN macrostrat.intervals tb ON strat_names_meta.b_int = tb.id
             ;
 
             UPDATE macrostrat.strat_name_footprints_new SET geom = 'POLYGON EMPTY' WHERE ST_GeometryType(geom) = 'ST_Point';
