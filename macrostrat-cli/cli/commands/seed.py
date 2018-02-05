0182a6abedac700eb32c3184ff17cb6dcc8f4250
John J Czaplewski <john@czaplewski.org>
2018-02-05 13:00:51 -0600

    Add seeding for carto-slim

--- macrostrat-cli/cli/commands/seed.py
+++ macrostrat-cli/cli/commands/seed.py
@@ -89,15 +89,22 @@ class Seed(Base):
             tiles = [ t for t in tilecover.cover_geometry(tiler, seed_area, z) ]
             headers = { 'X-Tilestrata-SkipCache': '*'}
             # Request that a tile be created
-            for layer in self.layers:
-                for tile in tqdm(tiles):
-                    url = 'http://localhost:5555/%s/%s/%s/%s.png' % (layer, tile.z, tile.x, tile.y)
-                    try:
-                        r = requests.get(url, headers=headers)
-                        if r.status_code != 200 and r.status_code != 204:
-                            print r.status_code
-                    except:
-                        pass
+            for tile in tqdm(tiles):
+                url = 'http://localhost:5555/carto/%s/%s/%s.png' % (tile.z, tile.x, tile.y)
+                try:
+                    r = requests.get(url, headers=headers)
+                    if r.status_code != 200 and r.status_code != 204:
+                        print r.status_code
+                except:
+                    pass
+
+                url = 'http://localhost:5555/carto-slim/%s/%s/%s.mvt' % (tile.z, tile.x, tile.y)
+                try:
+                    r = requests.get(url, headers=headers)
+                    if r.status_code != 200 and r.status_code != 204:
+                        print r.status_code
+                except:
+                    pass
 
         print '     Deleting...'
         for z in self.cached_zooms:
