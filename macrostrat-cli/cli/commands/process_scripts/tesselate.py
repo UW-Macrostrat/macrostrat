7600eec6a19c7b78a28367932a7898a7dc2fd92a
John J Czaplewski <john@czaplewski.org>
2018-02-16 16:08:01 -0600

    This isn't js

--- macrostrat-cli/cli/commands/process_scripts/tesselate.py
+++ macrostrat-cli/cli/commands/process_scripts/tesselate.py
@@ -409,7 +409,7 @@ class Tesselate(Base):
                 print 'When only one column is provided, a valid `buffer` or `snap_to_nearest` must also be provided'
                 sys.exit(1)
         else:
-            if len(columns) === 3:
+            if len(columns) == 3:
                 columns.append({'lng': 0, 'lat': 0})
             # Create the tesselation; initially open-ended and not clipped to the clipping polygon
             tesselation = Voronoi(np.array( [ [float(p['lng']), float(p['lat'])] for p in columns ] ))
