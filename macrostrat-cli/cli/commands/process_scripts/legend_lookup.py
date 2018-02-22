6380989686a9af0d6d56e5750ba312f5f7b7e115
John J Czaplewski <john@czaplewski.org>
2018-02-22 10:46:18 -0600

    Fix typo

--- macrostrat-cli/cli/commands/process_scripts/legend_lookup.py
+++ macrostrat-cli/cli/commands/process_scripts/legend_lookup.py
@@ -538,7 +538,7 @@ class LegendLookup(Base):
                         SELECT DISTINCT ON (legend.name, b_interval, t_interval) legend.name, b_interval, t_interval
                         FROM maps.legend
                         JOIN maps.sources on legend.source_id = legend.source_id
-                        where scale = ANY(%(scale)s)
+                        where scale = ANY(%(scales)s)
                     ) sub ON sub.name = l.name AND sub.b_interval = l.b_interval AND sub.t_interval = l.t_interval
                     WHERE l.source_id = %(source_id)s
                     GROUP BY l.name, l.strat_name, l.age
