51d593e9c0e3bc9149565457c65f1164fc825a1c
Daven Quinn <dev@davenquinn.com>
2018-09-16 16:56:37 -0500

    Added table 'measuremeta'

--- macrostrat-cli/cli/commands/table_meta.py
+++ macrostrat-cli/cli/commands/table_meta.py
@@ -1114,7 +1114,7 @@ tables = OrderedDict({
     },
 })
 
-from os import path, walk
+from os import path, listdir
 
 __here__ = path.dirname(__file__)
 __table_meta__ = path.join(__here__,"table_meta")
@@ -1122,20 +1122,21 @@ __table_meta__ = path.join(__here__,"table_meta")
 # Walk a directory tree and assemble a listing of SQL files
 # Note: this is inefficient and should eventually be replaced with
 # a function supporting the on-demand loading of SQL
-for (dirpath, dirnames, filenames) in walk(__table_meta__):
-    if not len(dirnames) == 1:
+for dirname in listdir(__table_meta__):
+    __dirpath__ = path.join(__table_meta__, dirname)
+    if not path.isdir(__dirpath__):
         continue
-    table_name = dirnames[0]
-    for fn in filenames:
-        if table_name not in tables:
-            tables[table_name] = OrderedDict({"process": ""})
+    table_name = dirname
+    if table_name not in tables:
+        tables[table_name] = OrderedDict({"process": ""})
+    for fn in listdir(__dirpath__):
         (base, ext) = path.splitext(fn)
         # ix is a prefix that shows ordering, 
         (ix, operation) = base.split("-")
         # Only accept SQL files (this way we can keep notes, etc.)
-        if ext != '.sql'
+        if ext != '.sql':
             continue
-        fp = path.join(__table_meta__,table,fn)
+        fp = path.join(__dirpath__,fn)
         with open(fp,'r') as f:
             sqltext = f.read()
         tables[table_name][operation] = sqltext
--- macrostrat-cli/cli/commands/table_meta/measurements/1-create.sql
+++ macrostrat-cli/cli/commands/table_meta/measurements/1-create.sql
@@ -1,9 +1,10 @@
-DROP TYPE IF EXISTS measurement_class CASCADE;
+DROP TYPE IF EXISTS measurement_class_new CASCADE;
+DROP TYPE IF EXISTS measurement_type_new CASCADE;
 CREATE TYPE measurement_class_new AS ENUM('','geophysical','geochemical','sedimentological');
 CREATE TYPE measurement_type_new AS ENUM('','material properties','geochronological','major elements','minor elements','radiogenic isotopes','stable isotopes','petrologic','environmental');
 CREATE TABLE macrostrat.measurements_new (
   id serial PRIMARY KEY NOT NULL,
-  measurement_class measurement_class,
-  measurement_type measurement_type,
+  measurement_class measurement_class_new,
+  measurement_type measurement_type_new,
   measurement text
 );
