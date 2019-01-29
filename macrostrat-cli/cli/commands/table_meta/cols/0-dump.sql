d78defeb266bb7f519bcef5e1bed8e189c97479d
John J Czaplewski <john@czaplewski.org>
2019-01-29 12:45:57 -0600

    Add col_type to cols table

--- macrostrat-cli/cli/commands/table_meta/cols/0-dump.sql
+++ macrostrat-cli/cli/commands/table_meta/cols/0-dump.sql
@@ -1,4 +1,3 @@
 
-SELECT id, col_group_id, project_id, status_code, col_position, col, col_name, lat, lng, col_area, ST_AsText(coordinate) AS wkt, created
+SELECT id, col_group_id, project_id, col_type, status_code, col_position, col, col_name, lat, lng, col_area, ST_AsText(coordinate) AS wkt, created
 FROM cols
-
--- macrostrat-cli/cli/commands/table_meta/cols/1-create.sql
+++ macrostrat-cli/cli/commands/table_meta/cols/1-create.sql
@@ -3,6 +3,7 @@ CREATE TABLE macrostrat.cols_new (
   id integer PRIMARY KEY,
   col_group_id smallint,
   project_id smallint,
+  col_type text,
   status_code character varying(25),
   col_position character varying(25),
   col numeric,
@@ -15,4 +16,3 @@ CREATE TABLE macrostrat.cols_new (
   created text,
   poly_geom geometry
 );
-
