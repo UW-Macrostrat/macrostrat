3797afe79308f7365fee82af7f1967ae9a935f7b
Shanan Peters <cambro@gmail.com>
2020-01-06 00:24:18 -0600

    do both tables

--- macrostrat-cli/cli/commands/rebuild_scripts/unit_boundaries.py
+++ macrostrat-cli/cli/commands/rebuild_scripts/unit_boundaries.py
@@ -13,12 +13,26 @@ class UnitBoundaries(Base):
         """)
 
         self.mariadb['cursor'].execute("""
+          UPDATE unit_boundaries_scratch ub
+          JOIN intervals i ON ub.t1 = i.id
+          SET ub.t1_age = age_bottom - ((age_bottom - age_top) * t1_prop)
+          WHERE boundary_status != 'absolute'
+        """)        
+        
+        self.mariadb['cursor'].execute("""
           UPDATE unit_boundaries ub
           JOIN intervals i ON ub.t1 = i.id
           SET ub.t1_prop = (age_bottom - t1_age)/(age_bottom - age_top)
           WHERE boundary_status = 'absolute'
         """)
 
+        self.mariadb['cursor'].execute("""
+          UPDATE unit_boundaries_scratch ub
+          JOIN intervals i ON ub.t1 = i.id
+          SET ub.t1_prop = (age_bottom - t1_age)/(age_bottom - age_top)
+          WHERE boundary_status = 'absolute'
+        """)        
+        
         self.mariadb['connection'].commit()
         self.mariadb['cursor'].close()
         self.mariadb['connection'].close()
