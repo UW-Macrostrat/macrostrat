ff627a6e2ca7f6f49ba079dbc2c83b08c2b11a90
Casey Idzikowski <idzikoj1234@gmail.com>
2021-06-02 22:18:44 -0500

    view created from map_face

--- services/column-footprint-editor/backend/fixtures/views.sql
+++ services/column-footprint-editor/backend/fixtures/views.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE VIEW columns.column_map_face AS
 WITH A as(
 SELECT c.id,c.project_id, c.col_id, c.col_name, c.col_group, mtm.geometry from map_topology.map_face mtm
 LEFT JOIN map_digitizer.polygon mdp
-ON ST_Intersects(mtm.geometry, mdp.geometry)
+ON ST_Contains(mtm.geometry, mdp.geometry)
 LEFT JOIN columns.columns c
-ON mdp.col_id = c.col_id
+ON mdp.col_id = c.id
 ) SELECT A.*, '#F95E5E' col_color from A
 WHERE A.col_id IS NULL
 UNION 
