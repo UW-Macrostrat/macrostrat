ed45315cf24aa5647be9018ef4b8e394173aa8f3
Casey Idzikowski <idzikoj1234@gmail.com>
2021-11-20 09:14:27 +0100

    multiline-string works for draggin, line tool not working yet

--- services/column-footprint-editor/backend/database/queries/get-linework.sql
+++ services/column-footprint-editor/backend/database/queries/get-linework.sql
@@ -1,1 +1,2 @@
-SELECT ST_AsGeoJSON(ST_MakeValid(ST_LineMerge(geometry))) lines, id from ${data_schema}.linework;
+-- SELECT ST_AsGeoJSON(ST_MakeValid(ST_LineMerge(geometry))) lines, id from ${data_schema}.linework;
+SELECT ST_AsGeoJSON(geometry) lines, id from ${data_schema}.linework;
