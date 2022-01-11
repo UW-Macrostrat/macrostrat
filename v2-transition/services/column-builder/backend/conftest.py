024c3f9c404bb3860a2fb27df01be0f45cbc7a64
Casey Idzikowski <idzikoj1234@gmail.com>
2022-01-10 19:17:37 -0600

    client testing with test db config

--- v2-transition/services/column-builder/backend/conftest.py
+++ v2-transition/services/column-builder/backend/conftest.py
@@ -1,14 +1,27 @@
 import pytest
-from psycopg import connect, rows
+from backend.app import app
+from backend.app.depends import get_db
+from backend.database import Database
 from backend.database.fixtures import get_sql
+from fastapi.testclient import TestClient
+
+# for testing the api
+## https://fastapi.tiangolo.com/tutorial/testing/
 
 testing_db = "postgresql://postgres@localhost:5434/col_test"
 
+def override_get_db():
+    db = Database(testing_db)
+    return db
+    
+# override db dependency connection
+## https://fastapi.tiangolo.com/advanced/testing-database/
+app.dependency_overrides[get_db] = override_get_db
+
 @pytest.fixture(scope="session")
 def connection():
-    conn = connect(testing_db, autocommit=True,row_factory=rows.dict_row)
-
-    return conn
+    db = Database(testing_db)
+    return db.conn
 
 @pytest.fixture(scope="session")
 def setup(connection):
@@ -24,3 +37,7 @@ def db(setup, connection):
     with connection.cursor() as cur:
         yield cur
     connection.rollback()    
+
+@pytest.fixture
+def client():
+    return TestClient(app)
