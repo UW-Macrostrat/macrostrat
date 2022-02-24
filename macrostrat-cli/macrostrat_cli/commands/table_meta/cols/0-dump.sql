d1119918f5161f6a520d0e8f4fe5f41cb58aebd7
Casey Idzikowski <idzikoj1234@gmail.com>
2022-02-24 16:22:57 +0100

    added notes field to cols and units table instead of separate joining table

--- macrostrat-cli/macrostrat_cli/commands/table_meta/cols/0-dump.sql
+++ macrostrat-cli/macrostrat_cli/commands/table_meta/cols/0-dump.sql
@@ -1,3 +1,19 @@
 
-SELECT id, col_group_id, project_id, col_type, status_code, col_position, col, col_name, lat, lng, col_area, ST_AsText(coordinate) AS wkt, created
-FROM cols
+SELECT 
+c.id, 
+col_group_id, 
+project_id, 
+col_type, 
+status_code, 
+col_position, 
+col, 
+col_name, 
+lat, 
+lng, 
+col_area, 
+ST_AsText(coordinate) AS wkt, 
+created,
+cn.notes
+FROM cols c 
+LEFT JOIN col_notes cn
+ON c.id = cn.col_id
--- macrostrat-cli/macrostrat_cli/commands/table_meta/units/0-dump.sql
+++ macrostrat-cli/macrostrat_cli/commands/table_meta/units/0-dump.sql
@@ -1,4 +1,23 @@
-
-SELECT id, strat_name, color, outcrop, FO, FO_h, LO, LO_h, position_bottom, position_top, max_thick, min_thick, section_id, col_id
-FROM units
-
+/* takes care of repeated  note fields*/
+WITH a AS(
+	SELECT unit_id, GROUP_CONCAT(DISTINCT notes SEPARATOR'; ') as notes from unit_notes group by unit_id 
+)
+SELECT 
+u.id, 
+u.strat_name, 
+u.color, 
+u.outcrop, 
+u.FO, 
+u.FO_h, 
+u.LO, 
+u.LO_h, 
+u.position_bottom, 
+u.position_top, 
+u.max_thick, 
+u.min_thick, 
+u.section_id, 
+u.col_id,
+a.notes as notes
+FROM units u
+LEFT JOIN a
+ON u.id = a.unit_id
