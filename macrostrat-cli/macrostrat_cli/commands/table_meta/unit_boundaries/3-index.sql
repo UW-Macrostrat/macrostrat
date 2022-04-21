ccd0923a85e366f90ae6af98afff6ad259806693
Casey Idzikowski <idzikoj1234@gmail.com>
2022-04-21 20:01:36 +0200

    unit_boundaries table

---
+++ macrostrat-cli/macrostrat_cli/commands/table_meta/unit_boundaries/3-index.sql
@@ -0,0 +1,4 @@
+CREATE INDEX on macrostrat.unit_boundaries (t1);
+CREATE INDEX on macrostrat.unit_boundaries (unit_id);
+CREATE INDEX on macrostrat.unit_boundaries (unit_id_2);
+CREATE INDEX on macrostrat.unit_boundaries (section_id);
