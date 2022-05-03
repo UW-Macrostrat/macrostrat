7019270c235b9dd07da7cb21c8e777b1656d6125
Casey Idzikowski <idzikoj1234@gmail.com>
2022-05-03 21:11:11 +0200

    combining and splitting sections

--- v2-transition/services/column-builder/api-views/02-functions.sql
+++ v2-transition/services/column-builder/api-views/02-functions.sql
@@ -42,3 +42,44 @@ RETURN QUERY
     ;
 END
 $$ LANGUAGE plpgsql;
+
+
+/* 
+Functions for Combing and Splitting Sections!
+*/
+CREATE OR REPLACE FUNCTION macrostrat_api.split_section(unit_ids int[])
+RETURNS VOID AS
+$$
+DECLARE
+  _col_id integer;
+  _section_id integer;
+BEGIN
+  SELECT col_id FROM macrostrat.units WHERE id = unit_ids[0] INTO _col_id;
+  INSERT INTO macrostrat.sections(col_id) VALUES (_col_id) RETURNING id INTO _section_id;
+  UPDATE macrostrat.units
+    SET 
+      section_id = _section_id
+    WHERE id = ANY(unit_ids); 
+END
+$$ language plpgsql;
+
+/* 
+Combine 2 or more sections
+*/
+CREATE OR REPLACE FUNCTION macrostrat_api.combine_sections(section_ids int[])
+RETURNS VOID AS
+$$
+BEGIN
+  IF array_length(section_ids, 1) < 2 THEN
+    RAISE EXCEPTION 'Not enough section_ids';
+  END IF;
+  -- arbitrarily choose first section_id to combine into
+  UPDATE macrostrat.units
+  SET
+    section_id = section_ids[1]
+  WHERE section_id = ANY(section_ids);
+  -- delete from sections table for rest of ids
+  DELETE FROM macrostrat.sections
+  WHERE id = ANY(section_ids[2:]);
+END
+$$ language plpgsql;
