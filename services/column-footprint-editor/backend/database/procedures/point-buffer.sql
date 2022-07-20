160750222f61d242c033d19c7b655f6cabcaaffb
Casey Idzikowski <idzikoj1234@gmail.com>
2022-07-19 20:11:54 -0500

    WIP trying to solve bug

--- services/column-footprint-editor/backend/database/procedures/point-buffer.sql
+++ services/column-footprint-editor/backend/database/procedures/point-buffer.sql
@@ -1,9 +1,12 @@
-WITH a AS (
+WITH a AS(
+    SELECT ST_Buffer(ST_GeomFromGeoJSON(:points), :radius, :quad_segs) as buffered_point
+),
+b AS (
     SELECT 
-        st_collect(geometry) as bounds
-    FROM ${topo_schema}.map_face
+        geometry as bounds
+    FROM ${topo_schema}.map_face, a WHERE st_intersects(geometry, a.buffered_point)
 ) SELECT
     st_asgeojson(st_dump(st_difference(
-        ST_Buffer(ST_GeomFromGeoJSON(:points), :radius, :quad_segs), 
-        a.bounds))) as buffered
-    FROM a;
+        st_snaptogrid(a.buffered_point, 0.001), 
+        b.bounds))) as buffered
+    FROM b,a;
