2dbdcaa76ad3d67a584585eba14ce071e6a21bac
Daven Quinn <dev@davenquinn.com>
2022-10-26 04:13:41 -0500

    Better descriptions

--- macrostrat-cli/macrostrat_cli/v2_commands/upgrade_db.py
+++ macrostrat-cli/macrostrat_cli/v2_commands/upgrade_db.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 from macrostrat.utils import relative_path
 from rich import print
+
 from ..database import db
 
 
 def upgrade_db():
-    """Upgrade the database to the latest version"""
+    """Apply PostgreSQL database upgrades to bring Macrostrat to the v2 standard"""
 
-    print("Connected to [dim green]" + str(db.engine.url))
+    print(
+        f"Connected to [dim green]{db.engine.url.database}[/dim green] on port [dim green]{db.engine.url.port}[/dim green]"
+    )
 
     filename = Path(relative_path(__file__, "procedures", "update-srid.sql"))
     db.exec_sql(filename)
