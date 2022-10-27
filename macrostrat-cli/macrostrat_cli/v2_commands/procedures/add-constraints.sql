60badeffeb60815842cf2060b9c19fc4454e3391
Daven Quinn <dev@davenquinn.com>
2022-10-26 22:51:32 -0500

    Add constraint to maps.sources table

--- macrostrat-cli/macrostrat_cli/v2_commands/procedures/add-constraints.sql
+++ macrostrat-cli/macrostrat_cli/v2_commands/procedures/add-constraints.sql
@@ -3,3 +3,12 @@
 ALTER TABLE maps.sources
 ADD CONSTRAINT map_sources_name_key
 UNIQUE (primary_table);
+
+-- Ensure that the source ID key is correctly autoincrementing
+-- and restart with the correct value
+BEGIN;
+-- protect against concurrent inserts
+LOCK TABLE maps.sources IN EXCLUSIVE MODE;
+-- Update the sequence
+SELECT setval('maps.sources_source_id_seq', COALESCE((SELECT max(source_id)+1 FROM maps.sources), 1), false);
+COMMIT;
