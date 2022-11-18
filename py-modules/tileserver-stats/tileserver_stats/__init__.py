85c5d40513e43fb8f30d6e7304c720a5547bb703
Daven Quinn <dev@davenquinn.com>
2022-11-18 16:47:18 -0600

    Does everything and takes two minutes for steno

--- py-modules/tileserver-stats/tileserver_stats/__init__.py
+++ py-modules/tileserver-stats/tileserver_stats/__init__.py
@@ -5,6 +5,8 @@ from os import environ
 from rich import print
 from typer import Typer
 from dotenv import load_dotenv
+from sqlalchemy.sql import text
+from datetime import datetime
 
 load_dotenv()
 
@@ -14,10 +16,36 @@ app.command()
 
 
 @app.command()
-def create_tables():
+def run():
 
     tileserver_db = environ.get("TILESERVER_STATS_DATABASE")
     db = Database(tileserver_db)
+
+    # Run update
+    fn = Path(relative_path(__file__, "procedures")) / "run-update.sql"
+
+    # check query timing
+    conn = db.engine.connect()
+    n_results = 10000
+    start = datetime.now()
+    step = start
+    while n_results > 0:
+        res = conn.execute(fn.read_text()).first()
+        n_results = res.n_rows
+        conn.execute("COMMIT")
+        next_step = datetime.now()
+        dt = (next_step - step).total_seconds()
+        print(f"{res.last_row_id} ({dt*1000:.0f} ms)")
+        step = next_step
+
+    print(f"Total time: {datetime.now() - start}")
+
+
+@app.command()
+def reset():
+    tileserver_db = environ.get("TILESERVER_STATS_DATABASE")
+    db = Database(tileserver_db)
+    db.engine.execute("DROP SCHEMA IF EXISTS stats CASCADE")
 
     files = Path(relative_path(__file__, "schema")).glob("*.sql")
     files = list(files)
--- py-modules/tileserver-stats/tileserver_stats/schema/schema.sql
+++ py-modules/tileserver-stats/tileserver_stats/schema/schema.sql
@@ -1,24 +1,30 @@
-CREATE TABLE IF NOT EXISTS processing_status (
+CREATE SCHEMA IF NOT EXISTS stats;
+
+CREATE TABLE IF NOT EXISTS stats.processing_status (
   last_row_id integer NOT NULL,
   last_row_time timestamp without time zone DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS day_index (
+CREATE TABLE IF NOT EXISTS stats.day_index (
   layer text NOT NULL,
   ext text NOT NULL,
   referrer text,
   app text,
   app_version text,
-  date timestamp without time zone NOT NULL
+  date timestamp without time zone NOT NULL,
+  num_requests integer NOT NULL,
+  UNIQUE (layer, ext, referrer, app, app_version, date)
 );
 
-CREATE TABLE IF NOT EXISTS location_index (
+CREATE TABLE IF NOT EXISTS stats.location_index (
   layer text NOT NULL,
   ext text NOT NULL,
   x integer NOT NULL,
   y integer NOT NULL,
   z integer NOT NULL,
-  orig_z integer
+  orig_z integer,
+  num_requests integer NOT NULL,
+  UNIQUE (layer, ext, x, y, z, orig_z)
 );
 
-INSERT INTO processing_status (last_row_id) VALUES (0);
+INSERT INTO stats.processing_status (last_row_id) VALUES (0);
