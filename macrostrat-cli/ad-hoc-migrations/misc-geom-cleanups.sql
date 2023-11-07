1344fae7e4613659949b8942c3260a34fe31c478
Daven Quinn <dev@davenquinn.com>
2023-11-06 22:56:21 -0600

    Delete invalid geometries

--- macrostrat-cli/ad-hoc-migrations/misc-geom-cleanups.sql
+++ macrostrat-cli/ad-hoc-migrations/misc-geom-cleanups.sql
@@ -2,3 +2,5 @@
 UPDATE maps.lines SET geom = ST_LineFromMultiPoint(geom)
 WHERE ST_GeometryType(geom) = 'ST_MultiPoint'
   AND NOT maps.lines_geom_is_valid(geom);
+
+DELETE FROM maps.lines WHERE NOT maps.lines_geom_is_valid(geom);
