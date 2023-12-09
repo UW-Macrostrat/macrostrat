17e4468e49076f886891491392d3306841272d4a
Daven Quinn <dev@davenquinn.com>
2023-12-09 03:58:08 -0600

    Temporarily remove complex caching functions

--- macrostrat-cli/macrostrat_cli/fixtures/05-corelle-macrostrat.sql
+++ macrostrat-cli/macrostrat_cli/fixtures/05-corelle-macrostrat.sql
@@ -30,12 +30,7 @@ SELECT
 	p.map_id,
 	p.scale,
 	pp.model_id model_id,
-	pp.plate_id,
-	CASE WHEN ST_Covers(pp.geometry, ST_Union(p.geom)) THEN
-		NULL  
-	ELSE
-		ST_Intersection(pp.geometry, ST_Union(p.geom))
-	END AS geom
+	pp.plate_id
 FROM carto.polygons p
 JOIN corelle.plate_polygon pp
   ON ST_Intersects(pp.geometry, p.geom)
@@ -47,7 +42,6 @@ ALTER TABLE corelle_macrostrat.carto_plate_index
 ADD CONSTRAINT carto_plate_index_pkey PRIMARY KEY (map_id, scale, model_id, plate_id);
 
 CREATE INDEX carto_plate_index_model_plate_scale_idx ON corelle_macrostrat.carto_plate_index(model_id, plate_id, scale);
-CREATE INDEX carto_plate_index_geom_idx ON corelle_macrostrat.carto_plate_index USING gist (geom);
 
 CREATE OR REPLACE FUNCTION corelle_macrostrat.tile_envelope(
   rotation numeric[],
@@ -173,27 +167,6 @@ EXCEPTION WHEN OTHERS THEN
   RETURN null;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
-
--- Adjust layers to have simplified geometries for rapid filtering
--- This should maybe be moved to Corelle
-ALTER TABLE corelle.plate_polygon ADD COLUMN geom_simple geometry(Geometry, 4326);
-ALTER TABLE corelle.rotation_cache ADD COLUMN geom geometry(Geometry, 4326);
-
-UPDATE corelle.plate_polygon
-SET geom_simple = corelle_macrostrat.antimeridian_split(ST_Multi(ST_Simplify(ST_Buffer(geometry, 0.1), 0.1)))
-WHERE geom_simple IS NULL;
-
-/** This isn't properly a "schema update". It is a required cache-filling operation. But it isn't
-    necessarily correct practice to run it every time the schema is regenerated. */
-UPDATE corelle.rotation_cache rc SET
-  geom = corelle_macrostrat.rotate(geom_simple, rotation, true)
-FROM corelle.plate_polygon pp
-WHERE pp.model_id = rc.model_id
-  AND pp.plate_id = rc.plate_id
-  AND geom IS null;
-
-CREATE INDEX rotation_cache_geom_idx ON corelle.rotation_cache USING gist (geom);
-
 
 -- Drop outdated functions
 DROP FUNCTION IF EXISTS corelle_macrostrat.rotate(geometry, numeric[], boolean);
