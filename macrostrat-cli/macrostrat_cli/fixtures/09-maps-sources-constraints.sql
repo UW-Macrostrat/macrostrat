198f6bd7c852ad1c6b472a463820c10fb6fd88ab
Daven Quinn <code@davenquinn.com>
2024-01-05 03:34:22 -0500

    Add map source operations table

--- macrostrat-cli/macrostrat_cli/fixtures/09-maps-sources-constraints.sql
+++
@@ -1,6 +0,0 @@
-/** Create primary keys for maps tables */
-
-ALTER TABLE maps.sources ADD PRIMARY KEY (source_id);
--- Make slug not null and unique (Postgres)
-ALTER TABLE maps.sources ALTER COLUMN slug SET NOT NULL;
-ALTER TABLE maps.sources ADD UNIQUE (slug);
