1c0aca8e00d50e68a94bc48c6da1ef6bb39249f7
Daven Quinn <code@davenquinn.com>
2024-01-17 11:48:33 -0600

    Move seed command to archive

--- entrypoint/macrostrat/cli/commands/__init__.py
+++ entrypoint/macrostrat/cli/commands/__init__.py
@@ -1,7 +1,6 @@
-from .rebuild import Rebuild as rebuild
-from .schlep import Schlep as schlep
-from .process import Process as process
-from .match import Match as match
-from .seed import Seed as seed
 from .backup import Backup as backup
 from .export import Export as export
+from .match import Match as match
+from .process import Process as process
+from .rebuild import Rebuild as rebuild
+from .schlep import Schlep as schlep
