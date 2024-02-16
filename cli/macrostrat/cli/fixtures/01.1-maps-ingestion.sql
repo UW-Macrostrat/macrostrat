3b044f52017edc19065b90a64b29c65b13336a40
Daven Quinn <code@davenquinn.com>
2024-02-16 00:14:16 -0600

    Moved maps ingestion table

---
+++ cli/macrostrat/cli/fixtures/01.1-maps-ingestion.sql
@@ -0,0 +1,5 @@
+/** Create a view to mirror this table in the correct location
+  * in order to keep the macrostrat schema clean and dedicated to stratigraphy.
+  */
+CREATE VIEW maps.ingest_process AS
+SELECT * FROM macrostrat.ingest_process;
