0735cccb7b6ac7f7a4f8c03e9dcbb1fe3329e3ef
Brian Aydemir <baydemir@morgridge.org>
2024-03-12 15:23:19 -0500

    Add options for metadata, assorted bug fixes

--- map-integration/macrostrat/map_integration/pipeline.py
+++ map-integration/macrostrat/map_integration/pipeline.py
@@ -7,19 +7,22 @@ import hashlib
 import pathlib
 import tempfile
 import zipfile
-from typing import Optional
+from typing import Annotated, Optional
 
 import magic
 import minio
 from macrostrat.core.schemas import (  # type: ignore[import-untyped]
     IngestProcess,
+    IngestState,
     Object,
     ObjectGroup,
     SchemeEnum,
     Sources,
 )
+from rich.console import Console
 from sqlalchemy import and_, insert, select, update
 from sqlalchemy.orm import Session
+from typer import Argument, Option
 
 from macrostrat.map_integration import config
 from macrostrat.map_integration.commands.ingest import ingest_map
@@ -28,20 +31,13 @@ from macrostrat.map_integration.database import db as DB
 from macrostrat.map_integration.process.geometry import create_rgeom, create_webgeom
 from macrostrat.map_integration.utils.map_info import get_map_info
 
+console = Console()
+
 
 def get_db_session(expire_on_commit=False) -> Session:
     # NOTE: By default, let ORM objects persist past commits, and let
     # consumers manage concurrent updates.
     return Session(DB.engine, expire_on_commit=expire_on_commit)
-
-
-def get_object_loc(file: pathlib.Path) -> tuple[str, str]:
-    """
-    Return the bucket and key corresponding to the given local file.
-    """
-    bucket = config.S3_BUCKET
-    key = f"{config.S3_PREFIX}/{file.name}"
-    return (bucket, key)
 
 
 def get_object_by_id(id_: int) -> Optional[Object]:
@@ -93,6 +89,14 @@ def update_object(id_: int, **data) -> Object:
     return new_obj
 
 
+def get_ingest_process_by_id(id_: int) -> Optional[IngestProcess]:
+    with get_db_session() as session:
+        ingest_process = session.scalar(
+            select(IngestProcess).where(IngestProcess.id == id_)
+        )
+    return ingest_process
+
+
 def get_ingest_process_by_object_group_id(id_: int) -> Optional[IngestProcess]:
     with get_db_session() as session:
         ingest_process = session.scalar(
@@ -101,11 +105,9 @@ def get_ingest_process_by_object_group_id(id_: int) -> Optional[IngestProcess]:
     return ingest_process
 
 
-def create_ingest_process(**data) -> IngestProcess:
+def create_ingest_process() -> IngestProcess:
     with get_db_session() as session:
-        object_group = session.scalar(
-            insert(ObjectGroup).values(**data).returning(ObjectGroup)
-        )
+        object_group = session.scalar(insert(ObjectGroup).returning(ObjectGroup))
         if not object_group:
             raise RuntimeError("Failed to create a new object group")
         ingest_process = session.scalar(
@@ -132,6 +134,10 @@ def update_ingest_process(id_: int, **data) -> IngestProcess:
     return new_ingest_process
 
 
+def get_source_by_slug(slug: str) -> Sources:
+    return get_map_info(DB, slug)
+
+
 def update_source(id_: int, **data) -> Sources:
     with get_db_session() as session:
         new_source = session.scalar(
@@ -148,13 +154,58 @@ def update_source(id_: int, **data) -> Sources:
 
 
 def run_pipeline(
-    local_file: pathlib.Path,
-    slug: str,
-    replace_object: bool = False,
+    local_file: Annotated[
+        pathlib.Path,
+        Argument(help="Local file to ingest"),
+    ],
+    slug: Annotated[
+        str,
+        Argument(help="Macrostrat slug to use for this map"),
+    ],
+    name: Annotated[
+        Optional[str],
+        Option(help="The map's name"),
+    ] = None,
+    ref_title: Annotated[
+        Optional[str],
+        Option(help="The map's report's title"),
+    ] = None,
+    ref_authors: Annotated[
+        Optional[str],
+        Option(help="The map's report's authors"),
+    ] = None,
+    ref_year: Annotated[
+        Optional[str],
+        Option(help="The map's report's year"),
+    ] = None,
+    ref_source: Annotated[
+        Optional[str],
+        Option(help="The map's report's source"),
+    ] = None,
+    ref_isbn_or_doi: Annotated[
+        Optional[str],
+        Option(help="The map's report's ISBN or DOI"),
+    ] = None,
+    scale: Annotated[
+        str,
+        Option(help="The map's scale"),
+    ] = "large",
+    s3_bucket: Annotated[
+        str,
+        Option(help="The S3 bucket to upload this object to"),
+    ] = config.S3_BUCKET,
+    s3_prefix: Annotated[
+        str,
+        Option(help="The prefix, sans trailing slash, to use for this object's S3 key"),
+    ] = config.S3_PREFIX,
+    replace_object: Annotated[
+        bool,
+        Option(help="Replace the current version of this object"),
+    ] = False,
 ) -> Object:
-
-    # Step 1: Calculate the object's metadata.
-
+    """
+    Ingest a local file containing a map into Macrostrat.
+    """
     mime_type = magic.Magic(mime=True).from_file(local_file)
     hasher = hashlib.sha256()
     with open(local_file, mode="rb") as fp:
@@ -162,9 +213,13 @@ def run_pipeline(
             hasher.update(data)
     sha256_hash = hasher.hexdigest()
 
-    # Step 2: Upload the file.
+    console.print(f"Detected {mime_type} with SHA-256 {sha256_hash[:8]}...")
 
-    bucket, key = get_object_loc(local_file)
+    ## Upload the file.
+
+    bucket = s3_bucket
+    key = f"{s3_prefix}/{local_file.name}"
+    uploaded_obj = False
 
     if obj := get_object_by_loc(bucket, key):
         if not replace_object and obj.sha256_hash != sha256_hash:
@@ -172,15 +227,17 @@ def run_pipeline(
                 "Attempting to upload a different version of this object"
             )
 
-    if not obj or obj.sha256_hash != sha256_hash:
+    if not obj or obj.sha256_hash != sha256_hash or replace_object:
+        console.print(f"Uploading file to S3 ({bucket}/{key})")
         s3 = minio.Minio(
             config.S3_HOST,
             access_key=config.S3_ACCESS_KEY,
             secret_key=config.S3_SECRET_KEY,
         )
         s3.fput_object(bucket, key, str(local_file))
+        uploaded_obj = True
 
-    # Step 3: Create or retrieve the object's ingest process.
+    ## Create or retrieve the ingest process.
 
     if obj:
         ingest_process = get_ingest_process_by_object_group_id(obj.object_group_id)
@@ -188,10 +245,16 @@ def run_pipeline(
         ingest_process = create_ingest_process()
     if not ingest_process:
         raise RuntimeError("Failed to create or retrieve the object's ingest process")
-    if ingest_process.state == "ingested":
+    if uploaded_obj:
+        update_ingest_process(ingest_process.id, state=None)
+        ingest_process = get_ingest_process_by_id(ingest_process.id)
+    if ingest_process.state == IngestState.ingested:
+        console.print("Ingest pipeline has already been completed")
         return obj
+    console.print(f"Using ingest process ID {ingest_process.id}")
+    console.print(f"Using object group ID {ingest_process.object_group_id}")
 
-    # Step 4: Create or update the object's DB entry.
+    ## Create or update the object's DB entry.
 
     payload = {
         "object_group_id": ingest_process.object_group_id,
@@ -208,32 +271,46 @@ def run_pipeline(
         obj = update_object(obj.id, **payload)
     else:
         obj = create_object(**payload)
+    console.print(f"Using object ID {obj.id}")
 
-    # Step 5: Locate and ingest files.
+    ## Process anything that might have points, lines, and polygons.
 
     if local_file.name.endswith(".zip"):
-        tmp_dir_obj = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
-        tmp_dir = pathlib.Path(tmp_dir_obj.name)
+        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as td:
+            tmp_dir = pathlib.Path(td)
+            console.print(f"Extracting zip archive into {tmp_dir}")
 
-        with zipfile.ZipFile(local_file) as zf:
-            zf.extractall(path=tmp_dir)
-        shapefiles = list(tmp_dir.glob("**/*.shp"))
+            with zipfile.ZipFile(local_file) as zf:
+                zf.extractall(path=tmp_dir)
 
-        ingest_map(slug, shapefiles)
-
-        macrostrat_map = get_map_info(DB, slug)
-
-        prepare_fields(macrostrat_map, recover=False)
-        create_rgeom(macrostrat_map)
-        create_webgeom(macrostrat_map)
-
-        update_source(
-            macrostrat_map.id,
-            scale="large",
-        )
-        update_ingest_process(
-            ingest_process.id, state="ingested", source_id=macrostrat_map.id
-        )
-
+            ingest_map(slug, list(tmp_dir.glob("**/*.shp")))
+        macrostrat_map = get_source_by_slug(slug)
     else:
         raise RuntimeError("Unrecognized file format")
+
+    ## Prepare tables for human review.
+
+    console.print(f"Using source ID {macrostrat_map.id}")
+
+    metadata = {"scale": scale}
+    if name:
+        metadata["name"] = name
+    if ref_title:
+        metadata["ref_title"] = ref_title
+    if ref_authors:
+        metadata["authors"] = ref_authors
+    if ref_year:
+        metadata["ref_year"] = ref_year
+    if ref_source:
+        metadata["ref_source"] = ref_source
+    if ref_isbn_or_doi:
+        metadata["isbn_doi"] = ref_isbn_or_doi
+
+    update_source(macrostrat_map.id, **metadata)
+    update_ingest_process(ingest_process.id, source_id=macrostrat_map.id)
+    prepare_fields(macrostrat_map)
+    create_rgeom(macrostrat_map)
+    create_webgeom(macrostrat_map)
+    update_ingest_process(ingest_process.id, state=IngestState.ingested)
+
+    return obj
