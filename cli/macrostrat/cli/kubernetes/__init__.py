cf919e225152f12511093e4df34a77031bbce199
Daven Quinn <code@davenquinn.com>
2024-07-08 22:45:17 -0500

    Remove unused function

--- cli/macrostrat/cli/kubernetes/__init__.py
+++ cli/macrostrat/cli/kubernetes/__init__.py
@@ -1,27 +1,13 @@
 """
 Functions for working with a Macrostrat instance in a Kubernetes cluster.
 """
+
 from contextlib import contextmanager
 from os import environ
 from subprocess import run
 from typing import Optional
 import base64
 import json
-
-
-@contextmanager
-def database_tunnel(settings):
-    """
-    Create a tunnel to the remote database.
-    """
-    from portforward import forward
-    namespace = getattr(settings, "kube_namespace", None)
-    pod = getattr(settings, "pg_database_pod", None)
-    port = environ.get("PGPORT", "5432")
-    if pod is None:
-        raise Exception("No pod specified.")
-    with forward(namespace, pod, int(port), 5432):
-        yield
 
 
 def read_secret(text):
