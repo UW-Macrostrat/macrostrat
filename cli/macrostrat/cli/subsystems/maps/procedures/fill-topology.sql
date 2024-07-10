6a093fc1b917a9b3ba4b2a07b9a1eb141e153fc6
Daven Quinn <code@davenquinn.com>
2024-07-10 15:33:20 -0500

    Started updates to handle map topology

---
+++ cli/macrostrat/cli/subsystems/maps/procedures/fill-topology.sql
@@ -0,0 +1,20 @@
+SET search_path TO map_bounds, maps, public;
+
+DELETE FROM map_bounds.map_layer WHERE name = 'Default';
+DELETE FROM map_bounds.linework_type WHERE name = 'Default';
+
+INSERT INTO map_bounds.map_layer (id, name, description, topological) VALUES
+    (1, 'map_bounds', 'Map boundary rgeom', TRUE)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO map_bounds.linework_type (id, name) VALUES
+    ('map_bounds', 'Map boundary')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO map_bounds.map_layer_linework_type (map_layer, type)
+VALUES (1, 'map_bounds');
+
+INSERT INTO map_bounds.linework (geometry, source_id, type, map_layer)
+SELECT ST_Boundary(rgeom), source_id, 'map_bounds', 1
+  FROM maps.sources
+  WHERE rgeom IS NOT NULL;
