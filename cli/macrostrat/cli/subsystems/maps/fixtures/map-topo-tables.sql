8c5c270d337776290aabb10645990f9afdd90ba5
Daven Quinn <code@davenquinn.com>
2024-07-15 23:50:01 -0500

    Updated procedures to improve topology handling

--- cli/macrostrat/cli/subsystems/maps/fixtures/map-topo-tables.sql
+++ cli/macrostrat/cli/subsystems/maps/fixtures/map-topo-tables.sql
@@ -6,6 +6,11 @@ CREATE TABLE IF NOT EXISTS map_bounds.boundary (
 
 
 ALTER TABLE map_bounds.linework
-  ADD COLUMN IF NOT EXISTS source_id integer NOT NULL
+  ADD COLUMN IF NOT EXISTS source_id integer
     REFERENCES maps.sources(source_id)
       ON DELETE CASCADE;
+
+/** Disable triggers (there may be a better way to do this) */
+CREATE OR REPLACE FUNCTION map_bounds.triggers_enabled() RETURNS boolean AS $$
+  SELECT false;
+$$ LANGUAGE sql;
--- cli/macrostrat/cli/subsystems/maps/procedures/update-topology-row.sql
+++ cli/macrostrat/cli/subsystems/maps/procedures/update-topology-row.sql
@@ -1,11 +1,12 @@
+
 UPDATE {data_schema}.linework l
 SET
   topo = topology.toTopoGeom(
         l.geometry,
         :topo_name,
         {topo_schema}.__linework_layer_id(),
-        0.01
+        :tolerance
       ),
       geometry_hash = {topo_schema}.hash_geometry(l),
       topology_error = null
-WHERE l.source_id = :source_id;
+WHERE l.id = :id;
