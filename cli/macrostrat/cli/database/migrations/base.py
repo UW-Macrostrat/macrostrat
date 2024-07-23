35a028e505db3187bd571181e7320004e17daa19
Matthew Westphall <westphall@wisc.edu>
2024-07-23 13:04:50 -0500

    add Dockerfile for local db

--- cli/macrostrat/cli/database/migrations/base.py
+++ cli/macrostrat/cli/database/migrations/base.py
@@ -1,5 +1,6 @@
 from macrostrat.database import Database
-
+from pathlib import Path
+import inspect
 
 class Migration:
     """This will eventually be merged with the migration system in macrostrat.dinosaur"""
@@ -7,12 +8,23 @@ class Migration:
     name: str
     description: str
     subsystem: str
+    expected_tables : list[str] = []
 
     def should_apply(self, database: Database):
-        raise NotImplementedError
+        """ Determine whether this migration needs to be run. By default, check that every table in
+        `self.expected_tables` exists. """
+        insp = database.inspector
+        for table_name in self.expected_tables:
+            table, schema = table_name.split('.')
+            if not insp.has_table(table, schema=schema):
+                return True
+        return False
 
     def apply(self, database: Database):
-        raise NotImplementedError
+        """ Apply the migrations defined by this class. By default, run every sql file 
+        in the same directory as the class definition. """
+        __dir__ = Path(inspect.getfile(self.__class__)).parent
+        database.run_fixtures(__dir__)
 
     def is_satisfied(self, database: Database):
         """In some cases, we may want to note that a migration does not need to be run
--- cli/macrostrat/cli/database/migrations/baseline/__init__.py
+++ cli/macrostrat/cli/database/migrations/baseline/__init__.py
@@ -1,23 +1,10 @@
 from ..base import Migration
-from macrostrat.database import Database
-from pathlib import Path
-
-__dir__ = Path(__file__).parent
-
 
 class BaselineMigration(Migration):
     name = "baseline"
-    # This partition is required
-    subsystem = "maps"
+    subsystem = "base"
     description = """
     Starting from an empty database, create the baseline macrostrat schemas as of 2023-08-29. 
     """
-
-    def should_apply(self, db: Database):
-        # Basic sanity check, check if the carto schema exist
-        insp = db.inspector
-        return not insp.has_schema("carto")
-
-    def apply(self, db: Database):
-        # We should sync this with the 'engine' configuration
-        db.run_fixtures(__dir__)
+    # Basic sanity check, just confirm that the first table created in the migration is present
+    expected_tables = ["carto.flat_large"]
