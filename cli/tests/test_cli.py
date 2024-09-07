2226fced5f49f0ee222dac5cc5b97bbdc90ec940
Daven Quinn <code@davenquinn.com>
2024-09-07 01:06:44 -0500

    Ensure all tests pass

--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -2,7 +2,6 @@
 
 from typer.testing import CliRunner
 
-from macrostrat.cli import main
 from pathlib import Path
 from macrostrat.utils import override_environment
 
@@ -14,13 +13,32 @@ __here__ = Path(__file__).parent
 def test_cli_help():
     cfg_file = __here__ / "macrostrat.test.toml"
     with override_environment(MACROSTRAT_CONFIG=str(cfg_file)):
+        from macrostrat.cli import main
+        from macrostrat.core.config import settings
+
+        assert cfg_file == settings.config_file
+
         result = runner.invoke(main, [])
         assert result.exit_code == 0
         assert "Macrostrat control interface" in result.output
 
 
+def test_cli_database():
+    cfg_file = __here__ / "macrostrat.test.toml"
+    with override_environment(MACROSTRAT_CONFIG=str(cfg_file)):
+        from macrostrat.cli import main
+        from macrostrat.core.config import settings
+
+        assert (
+            settings.pg_database
+            == "postgresql://user:password@localhost:5432/macrostrat"
+        )
+
+
 def test_cli_no_config():
     with override_environment(MACROSTRAT_CONFIG=""):
+        from macrostrat.cli import main
+
         result = runner.invoke(main, [])
         assert result.exit_code == 0
         assert "Macrostrat control interface" in result.output
