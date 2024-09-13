504d96d1bd8ba8614d7879aa0f4d2d5ffb57ce12
Daven Quinn <code@davenquinn.com>
2024-09-13 00:06:20 -0500

    Removed older versions of knowledge graph schemas

--- cli/macrostrat/cli/subsystems/knowledge_graph/fixtures/xdd-schema.sql
+++ cli/macrostrat/cli/subsystems/knowledge_graph/fixtures/xdd-schema.sql
@@ -1,1 +1,7 @@
-CREATE OR REPLACE VIEW macrostrat_api.xdd_model
+DROP VIEW IF EXISTS macrostrat_api.strat_names_units_kg;
+DROP VIEW IF EXISTS macrostrat_api.strat_names_ext;
+DROP VIEW IF EXISTS macrostrat_api.unit_liths_agg;
+DROP VIEW IF EXISTS macrostrat_api.strat_name_kg_relationships;
+
+DROP VIEW IF EXISTS macrostrat_kg.strat_name_kg_liths;
+DROP VIEW IF EXISTS macrostrat_kg.relationships_meta;
