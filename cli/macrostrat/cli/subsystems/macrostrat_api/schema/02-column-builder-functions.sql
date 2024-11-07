253928ed9b45bcc370ac1e80fdb4822d51475935
Daven Quinn <code@davenquinn.com>
2024-11-07 02:47:06 -0600

    Updated column builder views

--- cli/macrostrat/cli/subsystems/macrostrat_api/schema/02-column-builder-functions.sql
+++ cli/macrostrat/cli/subsystems/macrostrat_api/schema/02-column-builder-functions.sql
@@ -7,43 +7,6 @@
     Likewise bottom interval is unit.fo where unit.position_bottom is greatest!
  */
 DROP FUNCTION IF EXISTS macrostrat_api.get_col_section_data(integer);
-CREATE OR REPLACE FUNCTION macrostrat_api.get_col_section_data(column_id integer)
-RETURNS TABLE (
-    id integer,
-    unit_count integer,
-    top varchar(200),
-    bottom varchar(200)
-)
-AS
-$$
-BEGIN
-RETURN QUERY
-    SELECT
-        s.id::integer,
-        count(uc)::integer as unit_count,
-        lo.interval_name as top,
-        fo.interval_name as bottom
-    FROM macrostrat.sections s
-    JOIN macrostrat.units uc
-      ON uc.section_id = s.id
-    JOIN macrostrat.units u
-      ON u.section_id = s.id
-    JOIN macrostrat.units un
-      ON un.section_id = s.id
-    JOIN macrostrat.intervals fo
-      ON un.fo = fo.id
-    JOIN macrostrat.intervals lo
-      ON u.lo = lo.id
-    WHERE u.position_bottom = (
-        SELECT MIN(position_bottom) FROM macrostrat.units WHERE section_id = u.section_id
-    ) AND un.position_bottom = (
-        SELECT MAX(position_bottom) FROM macrostrat.units WHERE section_id = un.section_id
-    ) AND s.col_id = column_id
-    GROUP BY s.id, lo.interval_name, fo.interval_name, fo.age_bottom ORDER BY fo.age_bottom
-    ;
-END
-$$ LANGUAGE plpgsql;
-
 
 /** New (2024-11-07) create a view that is more efficient and flexible than the above function */
 CREATE OR REPLACE VIEW macrostrat_api.col_section_data AS
