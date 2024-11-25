916fce83a8bb7babb30c6eb40042c7878021ee3d
Daven Quinn <code@davenquinn.com>
2024-11-25 06:15:11 -0600

    Streamline error handling

--- core/macrostrat/core/exc.py
+++ core/macrostrat/core/exc.py
@@ -1,29 +1,12 @@
-from sys import stderr
-
+from macrostrat.utils import ApplicationError
 from rich.padding import Padding
 from typer import Typer
-
-from macrostrat.app_frame import ApplicationError
 
 from .console import err_console
 
 
 class MacrostratError(ApplicationError):
     """Base class for exceptions in this module."""
-
-    message: str
-    details: str | None
-
-    def __init__(self, message: str, details: str | None = None):
-        self.message = message
-        self.details = details
-        super().__init__(message)
-
-    def render(self):
-        repr = f"[danger]{self.message}[/]\n"
-        if self.details:
-            repr += f"[details]{self.details}[/]\n"
-        return repr
 
 
 ## We should standardize this
