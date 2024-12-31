23c310a412ea80876bbcf6114d68409678852af0
Daven Quinn <code@davenquinn.com>
2024-12-31 12:43:00 -0500

    Test of field domain values

--- map-integration/macrostrat/map_integration/test_geodatabase.py
+++ map-integration/macrostrat/map_integration/test_geodatabase.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 
+from osgeo import gdal, ogr
 from pytest import fixture
 
 from map_integration.commands.ingest import create_dataframe_for_layer
@@ -18,8 +19,6 @@ def gdb_fn():
 
 
 def test_get_geodatabase_domains(gdb_fn):
-    from osgeo import gdal, ogr
-
     ds = ogr.Open(str(gdb_fn))
     res = gdal.VectorInfo(ds, format="json")
 
@@ -54,6 +53,97 @@ def test_get_geodatabase_domains(gdb_fn):
             assert field["alias"] == "Epoca (maxima)"
             domain_name = field.get("domainName", None)
             assert domain_name is not None
+            assert domain_name == "LV_ATT_EPOCA"
             domain = domains[domain_name]
             assert domain["type"] == "coded"
             assert domain["codedValues"] is not None
+
+
+def get_domains_for_field(gdb, layer):
+
+    ds = ogr.Open(str(gdb))
+    res = gdal.VectorInfo(ds, format="json")
+
+    df = create_dataframe_for_layer(gdb, layer)
+
+    assert df is not None
+    assert len(df) > 150
+    assert "geometry" in df.columns
+
+    assert "domains" in res
+
+    domains = res["domains"]
+
+    lyr = None
+    for l in res["layers"]:
+        if l["name"] == layer:
+            lyr = l
+            break
+
+    assert lyr is not None
+
+    fields = lyr["fields"]
+
+    for field in fields:
+        assert field["name"] in df.columns
+
+        if field["name"] == "EPOCA_ESTRAT_MAX":
+            assert field["alias"] == "Epoca (maxima)"
+            domain_name = field.get("domainName", None)
+            assert domain_name is not None
+            assert domain_name == "LV_ATT_EPOCA"
+            domain = domains[domain_name]
+            assert domain["type"] == "coded"
+            assert domain["codedValues"] is not None
+
+    return df
+
+
+def get_layer_info(gdb, layer):
+    ds = ogr.Open(str(gdb))
+    res = gdal.VectorInfo(ds, format="json")
+
+    lyr = None
+    for l in res["layers"]:
+        if l["name"] == layer:
+            lyr = l
+            break
+
+    assert lyr is not None
+
+    for field in lyr["fields"]:
+        domains = res["domains"]
+
+        if domain_name := field.get("domainName", None):
+            domain = domains[domain_name]
+            if domain["type"] == "coded":
+                field["codedValues"] = domain["codedValues"]
+
+    # Recast fields as a dictionary
+    lyr["fields"] = {f["name"]: f for f in lyr["fields"]}
+
+    return lyr
+
+
+def test_apply_domains(gdb_fn):
+    layer = "GB_UNIDAD_GEO_P"
+    df = create_dataframe_for_layer(gdb_fn, layer)
+    info = get_layer_info(gdb_fn, layer)
+
+    field = info["fields"]["EPOCA_ESTRAT_MAX"]
+    assert field["domainName"] == "LV_ATT_EPOCA"
+    assert len(field["codedValues"]) > 0
+
+    for col in df.columns:
+        if col in info["fields"]:
+            field = info["fields"][col]
+            if "codedValues" in field:
+                df[col] = df[col].apply(lambda x: field["codedValues"].get(x, x))
+            if "alias" in field:
+                df = df.rename(columns={col: field["alias"]})
+
+    assert "Epoca (maxima)" in df.columns
+    assert "Epoca (minima)" in df.columns
+    assert "Tipo de Unidad" in df.columns
+
+    assert "Eoceno" in df["Epoca (maxima)"].unique()
