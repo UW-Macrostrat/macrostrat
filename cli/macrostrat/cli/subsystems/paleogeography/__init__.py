e439e27b7207b018ac33188ff1fa92ae7537760d
Daven Quinn <code@davenquinn.com>
2025-01-07 21:05:17 -0500

    Adjusted loading of paleogeography subsystem

--- cli/macrostrat/cli/subsystems/paleogeography/__init__.py
+++ cli/macrostrat/cli/subsystems/paleogeography/__init__.py
@@ -3,11 +3,15 @@ from sys import stderr
 
 from typer import Context, Typer
 
-from ...database import get_db
 from .corelle import build_carto_plate_index, create_corelle_fixtures
+from ...database import get_db
 
 
 def load_paleogeography_subsystem(app, main, db_subsystem):
+    if app.settings.pg_database is None:
+        print("No database configured, skipping corelle subsystem", file=stderr)
+        return app
+
     try:
         environ["CORELLE_DB"] = app.settings.pg_database
         from corelle.engine import cli as corelle_cli
