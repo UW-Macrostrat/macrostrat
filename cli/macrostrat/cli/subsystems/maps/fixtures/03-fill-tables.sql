83e7a57efce8ab50b2a03877fec20c5679d3fb88
Daven Quinn <code@davenquinn.com>
2025-01-09 15:33:16 -0500

    Add more multigeometry casting

--- cli/macrostrat/cli/subsystems/maps/fixtures/03-fill-tables.sql
+++ cli/macrostrat/cli/subsystems/maps/fixtures/03-fill-tables.sql
@@ -3,7 +3,7 @@ SET search_path TO map_bounds, maps, public;
 INSERT INTO map_bounds.map_area (source_id, geometry, area_km)
 SELECT
   source_id,
-  rgeom,
+  ST_Multi(rgeom),
   ST_Area(rgeom::geography) / 1e6
 FROM maps.sources
 WHERE rgeom IS NOT NULL
