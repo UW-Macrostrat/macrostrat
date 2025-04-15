97a0f6128cb6bf587d7d730f5e9918a826c19c82
Daven Quinn <code@davenquinn.com>
2025-04-15 14:58:49 -0500

    Migration tests work with fixture

--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -3,17 +3,21 @@
 import importlib
 from pathlib import Path
 
+import docker
+from macrostrat.database import Database
+from macrostrat.dinosaur.upgrade_cluster import database_cluster
+from macrostrat.utils import override_environment
 from pytest import fixture, mark
 from typer.testing import CliRunner
 
-from macrostrat.utils import override_environment
+from macrostrat.core.migrations import _run_migrations_in_database
 
 runner = CliRunner()
 
 __here__ = Path(__file__).parent
 
 
-@fixture
+@fixture(scope="session")
 def cfg():
     cfg_file = __here__ / "macrostrat.test.toml"
     with override_environment(MACROSTRAT_CONFIG=str(cfg_file), NO_COLOR="1"):
@@ -23,9 +27,27 @@ def cfg():
         assert cfg_file == settings.config_file
         yield settings
 
+
 @fixture(scope="session")
-def db():
-    pass
+def db(cfg):
+    # Spin up a docker container with a temporary database
+    image = cfg.get("pg_database_container", "postgres:15")
+
+    client = docker.from_env()
+
+    img_root = cfg.srcroot / "base-images" / "database"
+
+    # Build postgres pgaudit image
+    img_tag = "macrostrat-local-database:latest"
+
+    client.images.build(path=str(img_root), tag=img_tag)
+
+    # Spin up an image with this container
+    port = 54884
+    with database_cluster(client, img_tag, port=port) as container:
+        url = f"postgresql://postgres@localhost:{port}/postgres"
+        db = Database(url)
+        yield db
 
 
 def test_cli_help(cfg):
@@ -50,11 +72,10 @@ def test_cli_no_config():
 
 
 @mark.docker
-def test_database_migrations(cfg):
+def test_database_migrations(db):
     """Test that database migrations can be run."""
-    from macrostrat.core.migrations import _dry_run_migrations
 
-    res = _dry_run_migrations(legacy=False)
+    res = _run_migrations_in_database(db, legacy=False)
 
     assert res.n_migrations > 0
     assert res.n_remaining == 0
