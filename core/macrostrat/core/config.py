4d86b4f43d22fbdfafea2a95038d800f27e2c36a
Daven Quinn <code@davenquinn.com>
2025-04-27 03:56:50 -0500

    Improve config resolution and share it with env file resolution.

--- core/macrostrat/core/config.py
+++ core/macrostrat/core/config.py
@@ -69,11 +69,56 @@ def convert_to_string(value):
     return str(value)
 
 
+def list_of_paths(value):
+    if value is None:
+        return []
+    if isinstance(value, str):
+        return [Path(value)]
+    if isinstance(value, list):
+        return [Path(v) for v in value]
+    raise ValueError(f"Expected a string or list of strings, got {type(value)}")
+
+
+def list_of_resolved_paths(value, *, require_directory=False, require_file=False):
+    _paths = list_of_paths(value)
+    element_type = "file or directory"
+    if require_directory and require_file:
+        raise ValueError("Cannot require both files and directories")
+    if require_directory:
+        element_type = "directory"
+    if require_file:
+        element_type = "file"
+
+    _out = []
+    for p in _paths:
+        p1 = p.expanduser()
+        if not p1.is_absolute():
+            # Resolve relative to config file
+            if settings.config_file is not None:
+                p1 = settings.config_file.parent / p1
+        if not p1.exists():
+            raise ValueError(f"{p1} is not a valid {element_type}")
+        if require_directory and not p1.is_dir():
+            raise ValueError(f"{p1} is not a {element_type}")
+        if require_file and not p1.is_file():
+            raise ValueError(f"{p1} is not a {element_type}")
+        _out.append(p1.resolve())
+    return _out
+
+
+def list_of_directories(value):
+    return list_of_resolved_paths(value, require_directory=True)
+
+
+def list_of_files(value):
+    return list_of_resolved_paths(value, require_file=True)
+
+
 settings.validators.register(
     # `must_exist` is causing huge problems
     Validator("COMPOSE_ROOT", cast=Path),
-    Validator("env_files", cast=list[Path]),
-    Validator("script_dirs", cast=list[Path]),
+    Validator("env_files", cast=list_of_files, default=None),
+    Validator("script_dirs", cast=list_of_directories, default=None),
     Validator("pg_database", cast=convert_to_string, default=None),
     # Backend information. We could potentially infer this from other environment variables
     Validator("backend", default="kubernetes", cast=BackendType),
@@ -81,18 +126,11 @@ settings.validators.register(
 
 macrostrat_env = getattr(settings, "env", "default")
 
+# Actually load environment files
 if env_files := getattr(settings, "env_files", None):
     for env in env_files:
-        e = Path(env)
-        if not e.is_absolute():
-            # Resolve relative to config file
-            e = settings.config_file.parent / e
-
-        if not e.exists():
-            raise FileNotFoundError(f"Environment file {e} not found")
-
-        log.info(f"Loading environment variables from {e}")
-        load_dotenv(e)
+        log.info(f"Loading environment variables from {env}")
+        load_dotenv(env)
 
 
 settings.validators.validate()
