15026065695509db2cfed15937f6adf733f82b6b
davenquinn <1537910+davenquinn@users.noreply.github.com>
2025-05-20 05:36:49 +0000

    Format code and sort imports

--- map-integration/tests/test_map_staging.py
+++ map-integration/tests/test_map_staging.py
@@ -1,13 +1,15 @@
-from psycopg2.sql import Identifier
-from macrostrat.map_integration.commands.ingest import ingest_map
-from macrostrat.map_integration.utils.map_info import get_map_info
-from macrostrat.map_integration.process.geometry import create_rgeom, create_webgeom
-from macrostrat.map_integration.commands.prepare_fields import _prepare_fields
-from macrostrat.map_integration.utils.file_discovery import find_gis_files
 from pathlib import Path
+
 import pytest
+from psycopg2.sql import Identifier
 from sqlalchemy import create_engine, text
+
 from macrostrat.core.database import Database
+from macrostrat.map_integration.commands.ingest import ingest_map
+from macrostrat.map_integration.commands.prepare_fields import _prepare_fields
+from macrostrat.map_integration.process.geometry import create_rgeom, create_webgeom
+from macrostrat.map_integration.utils.file_discovery import find_gis_files
+from macrostrat.map_integration.utils.map_info import get_map_info
 
 
 def test_maps_tables_exist(db):
@@ -21,12 +23,15 @@ def test_maps_tables_exist(db):
 
 def test_get_database(db):
     from macrostrat.core.database import db_ctx
+
     db_ctx.set(db)
     from macrostrat.map_integration.database import get_database
+
     db1 = get_database()
     assert db1 is db
 
-'''
+
+"""
 @pytest.fixture(scope="session", autouse=True)
 def allow_macrostrat_login():
     super_engine = create_engine("postgresql://postgres@localhost:54884/postgres?sslmode=disable")
@@ -43,8 +48,7 @@ def db_as_macrostrat(allow_macrostrat_login):
     engine = create_engine(url)
     db = Database(engine)
     yield db
-    engine.dispose()'''
-
+    engine.dispose()"""
 
 
 @pytest.fixture
@@ -57,19 +61,15 @@ def test_maps():
     }
 
 
-
-
 def test_map_staging(db, test_maps):
     """
     Ingest a map, update metadata, prepare fields, and build geometries.
     """
-    #db = db_as_macrostrat
+    # db = db_as_macrostrat
     slug = test_maps["slug"]
     data_path = test_maps["data_path"]
     name = test_maps["name"]
     filter = test_maps["filter"]
-
-
 
     print(f"Ingesting {slug} from {data_path}")
 
@@ -108,9 +108,9 @@ def test_map_staging(db, test_maps):
         dict(scale="large", source_id=source_id),
     )
 
-    #object_group_id is a foreign key into the storage schema where the curr user postgres does not have access to.
-    #the storage.sql ALTER TABLE storage.object OWNER TO macrostrat is switching the owner.
-    #we are temporarily using macrostrat to run the query below
+    # object_group_id is a foreign key into the storage schema where the curr user postgres does not have access to.
+    # the storage.sql ALTER TABLE storage.object OWNER TO macrostrat is switching the owner.
+    # we are temporarily using macrostrat to run the query below
     object_group_id = db.run_query(
         "INSERT INTO storage.object_group DEFAULT VALUES RETURNING id"
     ).scalar()
@@ -122,7 +122,7 @@ def test_map_staging(db, test_maps):
         INSERT INTO maps_metadata.ingest_process (state, source_id, object_group_id)
         VALUES (:state, :source_id, :object_group_id);
         """,
-        dict(state='ingested', source_id=source_id, object_group_id=object_group_id),
+        dict(state="ingested", source_id=source_id, object_group_id=object_group_id),
     )
 
     map_info = get_map_info(db, slug)
@@ -130,7 +130,7 @@ def test_map_staging(db, test_maps):
     create_rgeom(map_info)
     create_webgeom(map_info)
 
-    #Metadata assertions
+    # Metadata assertions
     row = db.run_query(
         "SELECT name, scale FROM maps.sources WHERE source_id = :source_id",
         dict(source_id=source_id),
@@ -139,7 +139,7 @@ def test_map_staging(db, test_maps):
     assert row.name == name
     assert row.scale == "large"
 
-    #Ingest process assertions
+    # Ingest process assertions
     ingest_process = db.run_query(
         "SELECT source_id, object_group_id, state FROM maps_metadata.ingest_process WHERE source_id = :source_id",
         dict(source_id=source_id),
@@ -148,14 +148,11 @@ def test_map_staging(db, test_maps):
     assert ingest_process.state == "ingested"
     assert ingest_process.object_group_id == object_group_id
 
-    
-    #Data exists
-    count = db.run_query(
-        f"SELECT COUNT(*) FROM sources.{slug}_polygons"
-    ).scalar()
+    # Data exists
+    count = db.run_query(f"SELECT COUNT(*) FROM sources.{slug}_polygons").scalar()
     assert count > 0
 
-    #Geometry column assertions
+    # Geometry column assertions
     rgeom = db.run_query(
         """
         SELECT rgeom FROM maps.sources WHERE slug = :slug
@@ -173,7 +170,3 @@ def test_map_staging(db, test_maps):
     assert web_geom is not None
 
     print("All tests have passed the map ingestion staging test suite!")
-
-
-
-
