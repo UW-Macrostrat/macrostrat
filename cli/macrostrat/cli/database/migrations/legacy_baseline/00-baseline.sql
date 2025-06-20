1466780f7af591669c12bbc0eaf609809706bd1c
amyfromandi <amyfromandi@gmail.com>
2025-06-20 11:27:55 -0500

    added TODOs to improve processes

--- cli/macrostrat/cli/database/migrations/legacy_baseline/00-baseline.sql
+++ cli/macrostrat/cli/database/migrations/legacy_baseline/00-baseline.sql
@@ -2620,13 +2620,6 @@ CREATE TABLE public.lookup_tiny (
     legend_id integer
 );
 
---granting read only access for the rockd api to read macrostrat.public
-ALTER TABLE public.lookup_tiny OWNER TO postgres;
-GRANT USAGE ON SCHEMA public TO "rockd-reader";
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO "rockd-reader";
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT SELECT ON TABLES TO "rockd-reader";
-
 --
 -- Name: macrostrat_union; Type: TABLE; Schema: public; Owner: postgres
 --
