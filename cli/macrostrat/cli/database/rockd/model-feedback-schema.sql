6153ba7ad337e6f0c79d27a53fa04971696e752b
amyfromandi <amyfromandi@gmail.com>
2025-06-30 12:24:23 -0500

    updated FK/PK structure to avoid redundancy

--- cli/macrostrat/cli/database/rockd/model-feedback-schema.sql
+++ cli/macrostrat/cli/database/rockd/model-feedback-schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.model_feedback (
     id serial PRIMARY KEY,
-    checkin_id integer NOT NULL REFERENCES public.checkins(checkin_id),
-    observation_id integer NOT NULL REFERENCES public.observations(obs_id),
-    photo_id integer NOT NULL REFERENCES public.observations(photo),
+    checkin_id integer NOT NULL REFERENCES public.checkins(checkin_id) ON DELETE CASCADE,
+    observation_id integer NOT NULL REFERENCES public.observations(obs_id) ON DELETE CASCADE,
+    photo_id integer NOT NULL,
     text_relevancy float NOT NULL,
     image_relevancy float NOT NULL,
     text_appropriateness float NOT NULL,
@@ -10,21 +10,3 @@ CREATE TABLE IF NOT EXISTS public.model_feedback (
     status_code integer NOT NULL,
     date_created timestamp with time zone NOT NULL DEFAULT now()
 );
-
-ALTER TABLE public.observations
-    ADD COLUMN model_run_id integer;
-
-ALTER TABLE public.observations
-    ADD CONSTRAINT fk_model_run_observations
-        FOREIGN KEY (model_run_id)
-        REFERENCES public.model_feedback(id)
-        ON DELETE SET NULL;
-
-ALTER TABLE public.checkins
-    ADD COLUMN model_run_id integer;
-
-ALTER TABLE public.checkins
-    ADD CONSTRAINT fk_model_run_checkins
-        FOREIGN KEY (model_run_id)
-        REFERENCES public.model_feedback(id)
-        ON DELETE SET NULL;
