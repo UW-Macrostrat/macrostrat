dd8a5b1771ee58aa98716ff28b4e7153a2a653e1
davidsklar99 <davidsklar99@outlook.com>
2025-06-30 10:19:01 -0500

    Fixes

--- cli/macrostrat/cli/database/rockd/model-feedback-schema.sql
+++ cli/macrostrat/cli/database/rockd/model-feedback-schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.model_feedback (
     id serial PRIMARY KEY,
-    checkin_id integer NOT NULL,
-    observation_id integer,
-    photo_id integer NOT NULL,
+    checkin_id integer NOT NULL REFERENCES public.checkins(checkin_id),
+    observation_id integer NOT NULL REFERENCES public.observations(obs_id),
+    photo_id integer NOT NULL REFERENCES public.observations(photo),
     text_relevancy float NOT NULL,
     image_relevancy float NOT NULL,
     text_appropriateness float NOT NULL,
@@ -12,19 +12,7 @@ CREATE TABLE IF NOT EXISTS public.model_feedback (
 );
 
 ALTER TABLE public.observations
-    ADD COLUMN model_run_id integer;
-
-ALTER TABLE public.observations
-    ADD CONSTRAINT fk_model_run
-        FOREIGN KEY (model_run_id)
-        REFERENCES public.model_feedback(id)
-        ON DELETE SET NULL;
+    ADD COLUMN model_run_id integer REFERENCES public.model_feedback(id);
 
 ALTER TABLE public.checkins
-    ADD COLUMN model_run_id integer;
-
-ALTER TABLE public.checkins
-    ADD CONSTRAINT fk_model_run
-        FOREIGN KEY (model_run_id)
-        REFERENCES public.model_feedback(id)
-        ON DELETE SET NULL;
+    ADD COLUMN model_run_id integer REFERENCES public.model_feedback(id);
