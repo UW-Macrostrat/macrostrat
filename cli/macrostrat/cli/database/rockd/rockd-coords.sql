ea70a718e92bee4d5547c1d4fd69d5081c32ff27
davidsklar99 <davidsklar99@outlook.com>
2025-07-15 13:06:48 -0500

    Update structure

--- cli/macrostrat/cli/database/rockd/rockd-coords.sql
+++ cli/macrostrat/cli/database/rockd/rockd-coords.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS public.rockd_coords (
-    date_created timestamp with time zone NOT NULL DEFAULT now() PRIMARY KEY,
-    latitude float NOT NULL,
-    longitude float NOT NULL,
+CREATE TABLE IF NOT EXISTS public.usage_stats (
+    id serial PRIMARY KEY,
+    date timestamp with time zone NOT NULL DEFAULT now(),
+    ip text NOT NULL,
+    lat float NOT NULL,
+    lng float NOT NULL
 );
