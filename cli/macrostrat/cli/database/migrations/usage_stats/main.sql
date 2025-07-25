92f2411d511db22cf3295d6876fc9b5a19030f4c
davidsklar99 <davidsklar99@outlook.com>
2025-07-25 11:19:30 -0500

    Change table names and add tileserver stats

--- cli/macrostrat/cli/database/migrations/usage_stats/main.sql
+++ cli/macrostrat/cli/database/migrations/usage_stats/main.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA usage_stats;
 
-CREATE TABLE IF NOT EXISTS usage_stats.rockd (
+CREATE TABLE IF NOT EXISTS usage_stats.rockd_stats (
     id serial PRIMARY KEY,
     date timestamp with time zone NOT NULL DEFAULT now(),
     ip text NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS usage_stats.rockd (
     matomo_id integer NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS usage_stats.macrostrat (
+CREATE TABLE IF NOT EXISTS usage_stats.macrostrat_stats (
     id serial PRIMARY KEY,
     date timestamp with time zone NOT NULL DEFAULT now(),
     ip text NOT NULL,
@@ -17,3 +17,20 @@ CREATE TABLE IF NOT EXISTS usage_stats.macrostrat (
     lng float NOT NULL,
     matomo_id integer NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS usage_stats.tileserver_stats (
+    req_id serial PRIMARY KEY,
+    uri text NOT NULL,
+    layer text NOT NULL,
+    ext text NOT NULL,
+    x integer NOT NULL,
+    y integer NOT NULL,
+    z integer NOT NULL,
+    referrer text NOT NULL,
+    app text,
+    app_version text,
+    cache_hit boolean NOT NULL,
+    redis_hit boolean NOT NULL,
+    time timestamp without time zone NOT NULL
+);
+
