d9b95be5d19c51b1b583524f06a856c798fd64f1
amyfromandi <amyfromandi@gmail.com>
2025-07-25 15:55:22 -0500

    removed DEFAULT current_app_user_id()  in the user_id column

--- cli/macrostrat/cli/database/migrations/user_features/user_locations_schema.sql
+++ cli/macrostrat/cli/database/migrations/user_features/user_locations_schema.sql
@@ -6,7 +6,7 @@ create table IF NOT EXISTS user_features.user_locations
   --instead of serial for id, we want GENERATED ALWAYS AS IDENTITY so that postgrest can handle the id increments internally and
   --we don't have to rely on granting sequence id access to the base table to web_user.
   --Goal is to try to keep grant access to web_user on the views only rather than the base tables.
-  --removed DEFAULT current_app_user_id() 
+  --removed DEFAULT current_app_user_id()
     id              integer GENERATED ALWAYS AS IDENTITY primary key,
     user_id         integer not null constraint fk_user references macrostrat_auth."user" on delete cascade,
     name            varchar(120) not null,
