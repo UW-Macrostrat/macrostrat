c75ebde5f48a3478555b367683750ee7ed796659
davidsklar99 <davidsklar99@outlook.com>
2025-07-31 10:12:50 -0400

    Add image

--- cli/macrostrat/cli/database/migrations/people/main.sql
+++ cli/macrostrat/cli/database/migrations/people/main.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS macrostrat.people (
     email text NOT NULL UNIQUE,
     title text NOT NULL,
     website text,
+    img_id text,
     student boolean NOT NULL DEFAULT false,
     researcher boolean NOT NULL DEFAULT false,
     developer boolean NOT NULL DEFAULT false,
