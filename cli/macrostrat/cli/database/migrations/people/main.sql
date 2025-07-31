5147042d8c0bf9791d0025867ae035c223e5b1a8
davidsklar99 <davidsklar99@outlook.com>
2025-07-31 10:15:03 -0400

    Add contributions

--- cli/macrostrat/cli/database/migrations/people/main.sql
+++ cli/macrostrat/cli/database/migrations/people/main.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS macrostrat.people (
     research_scientist boolean NOT NULL DEFAULT false,
     former boolean NOT NULL DEFAULT false
 );
+
+CREATE TABLE IF NOT EXISTS macrostrat.contributions (
+    id serial PRIMARY KEY,
+    person_id integer NOT NULL REFERENCES macrostrat.people(id) ON DELETE CASCADE,
+    contribution text NOT NULL,
+    rockd boolean NOT NULL DEFAULT false,
+    macrostrat boolean NOT NULL DEFAULT false,
+    date_created timestamp with time zone NOT NULL DEFAULT now()
+);
