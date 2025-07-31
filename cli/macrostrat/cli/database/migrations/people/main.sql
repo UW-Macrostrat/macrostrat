aca54753a7131c9ae66f1d53316d4b7c106b1b12
davidsklar99 <davidsklar99@outlook.com>
2025-07-31 13:15:46 -0400

    Updates

--- cli/macrostrat/cli/database/migrations/people/main.sql
+++ cli/macrostrat/cli/database/migrations/people/main.sql
@@ -1,23 +1,35 @@
-CREATE TABLE IF NOT EXISTS macrostrat.people (
+CREATE SCHEMA ecosystem;
+
+CREATE TABLE IF NOT EXISTS ecosystem.people (
     id serial PRIMARY KEY,
     name text NOT NULL,
     email text NOT NULL UNIQUE,
     title text NOT NULL,
     website text,
     img_id text,
-    student boolean NOT NULL DEFAULT false,
-    researcher boolean NOT NULL DEFAULT false,
-    developer boolean NOT NULL DEFAULT false,
-    postdoc boolean NOT NULL DEFAULT false,
-    research_scientist boolean NOT NULL DEFAULT false,
-    former boolean NOT NULL DEFAULT false
+    roles integer[] NOT NULL DEFAULT '{}',
+    active_start timestamp with time zone NOT NULL DEFAULT now(),
+    active_end timestamp with time zone,
 );
 
-CREATE TABLE IF NOT EXISTS macrostrat.contributions (
+CREATE TABLE IF NOT EXISTS ecosystem.roles (
+    id serial PRIMARY KEY,
+    name text NOT NULL UNIQUE,
+    description text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ecosystem.people_roles (
+    person_id integer NOT NULL REFERENCES ecosystem.people(id) ON DELETE CASCADE,
+    role_id integer NOT NULL REFERENCES ecosystem.roles(id) ON DELETE CASCADE,
+    PRIMARY KEY (person_id, role_id)
+);
+
+CREATE TABLE IF NOT EXISTS ecosystem.contributions (
     id serial PRIMARY KEY,
     person_id integer NOT NULL REFERENCES macrostrat.people(id) ON DELETE CASCADE,
     contribution text NOT NULL,
     rockd boolean NOT NULL DEFAULT false,
     macrostrat boolean NOT NULL DEFAULT false,
-    date_created timestamp with time zone NOT NULL DEFAULT now()
+    date timestamp with time zone NOT NULL DEFAULT now(),
+    url text
 );
