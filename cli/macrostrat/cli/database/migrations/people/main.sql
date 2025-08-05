580b2e0b3c39ddb72f5874b61d502d15426a1213
davidsklar99 <davidsklar99@outlook.com>
2025-08-05 14:23:45 -0400

    Confirmed works people

--- cli/macrostrat/cli/database/migrations/people/main.sql
+++ cli/macrostrat/cli/database/migrations/people/main.sql
@@ -1,9 +1,10 @@
--- SCHEMA
+
+ -- SCHEMA
 CREATE SCHEMA IF NOT EXISTS ecosystem;
 
 -- PEOPLE
 CREATE TABLE IF NOT EXISTS ecosystem.people (
-    id serial PRIMARY KEY,
+    person_id serial PRIMARY KEY,
     name text NOT NULL,
     email text NOT NULL UNIQUE,
     title text NOT NULL,
@@ -15,22 +16,22 @@ CREATE TABLE IF NOT EXISTS ecosystem.people (
 
 -- ROLES
 CREATE TABLE IF NOT EXISTS ecosystem.roles (
-    id serial PRIMARY KEY,
+    role_id serial PRIMARY KEY,
     name text NOT NULL UNIQUE,
     description text NOT NULL
 );
 
 -- PEOPLE-ROLES MAPPING
 CREATE TABLE IF NOT EXISTS ecosystem.people_roles (
-    person_id integer NOT NULL REFERENCES ecosystem.people(id) ON DELETE CASCADE,
-    role_id integer NOT NULL REFERENCES ecosystem.roles(id) ON DELETE CASCADE,
+    person_id integer NOT NULL REFERENCES ecosystem.people(person_id) ON DELETE CASCADE,
+    role_id integer NOT NULL REFERENCES ecosystem.roles(role_id) ON DELETE CASCADE,
     PRIMARY KEY (person_id, role_id)
 );
 
 -- CONTRIBUTIONS
 CREATE TABLE IF NOT EXISTS ecosystem.contributions (
-    id serial PRIMARY KEY,
-    person_id integer NOT NULL REFERENCES ecosystem.people(id) ON DELETE CASCADE,
+    contribution_id serial PRIMARY KEY,
+    person_id integer NOT NULL REFERENCES ecosystem.people(person_id) ON DELETE CASCADE,
     contribution text NOT NULL,
     description text,
     date timestamp with time zone NOT NULL DEFAULT now(),
@@ -39,8 +40,8 @@ CREATE TABLE IF NOT EXISTS ecosystem.contributions (
 
 -- PEOPLE-CONTRIBUTIONS MAPPING
 CREATE TABLE IF NOT EXISTS ecosystem.people_contributions (
-    person_id integer NOT NULL REFERENCES ecosystem.people(id) ON DELETE CASCADE,
-    contribution_id integer NOT NULL REFERENCES ecosystem.contributions(id) ON DELETE CASCADE,
+    person_id integer NOT NULL REFERENCES ecosystem.people(person_id) ON DELETE CASCADE,
+    contribution_id integer NOT NULL REFERENCES ecosystem.contributions(contribution_id) ON DELETE CASCADE,
     PRIMARY KEY (person_id, contribution_id)
 );
 
@@ -53,6 +54,19 @@ INSERT INTO ecosystem.roles (name, description) VALUES
   ('Collaborator', 'Contributes to joint projects')
 ON CONFLICT (name) DO NOTHING;
 
+-- CREATE POSTGREST VIEWS
+CREATE OR REPLACE VIEW macrostrat_api.people AS
+	SELECT * FROM ecosystem.people;
+
+CREATE OR REPLACE VIEW macrostrat_api.people_roles AS
+	SELECT * FROM ecosystem.people_roles;
+
 -- DEFAULT PRIVILEGES
-GRANT SELECT, INSERT, UPDATE, DELETE ON ecosystem.people, macrostrat_api.people TO web_admin;
-GRANT USAGE, SELECT ON SEQUENCE ecosystem.people_id_seq TO web_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON 
+	ecosystem.people, 
+	ecosystem.people_roles,
+	macrostrat_api.people, 
+	macrostrat_api.people_roles
+TO web_admin;
+
+GRANT USAGE, SELECT ON SEQUENCE ecosystem.people_person_id_seq TO web_admin;
