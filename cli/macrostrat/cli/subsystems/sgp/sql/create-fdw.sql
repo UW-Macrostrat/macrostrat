e6519ffb2f57c46aeb912c1d8e6cbb23474d0a88
Daven Quinn <code@davenquinn.com>
2025-08-29 15:01:38 -0500

    Allow SGP data import via a foreign data wrapper

--- cli/macrostrat/cli/subsystems/sgp/sql/create-fdw.sql
+++ cli/macrostrat/cli/subsystems/sgp/sql/create-fdw.sql
@@ -1,13 +1,15 @@
 /** Create a foreign data wrapper for the SGP database */
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 
+DROP SERVER IF EXISTS sgp_server CASCADE;
 CREATE SERVER IF NOT EXISTS sgp_server
   FOREIGN DATA WRAPPER postgres_fdw
-  OPTIONS (host 'localhost', dbname 'sgp', port '5432');
+  OPTIONS (host :sgp_host, dbname :sgp_database, port :sgp_port);
 
 /** Create a user mapping for the SGP database */
 CREATE USER MAPPING IF NOT EXISTS FOR CURRENT_USER
-  SERVER sgp_server;
+  SERVER sgp_server
+  OPTIONS (user :sgp_user, password :sgp_password);
 
 CREATE SCHEMA IF NOT EXISTS sgp;
 IMPORT FOREIGN SCHEMA public FROM SERVER sgp_server INTO sgp;
