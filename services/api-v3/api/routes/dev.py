440ac6a980629027615b368a14e48dc4f2a72705
amyfromandi <83303019+amyfromandi@users.noreply.github.com>
2025-11-06 19:14:52 +0000

    Format code and sort imports

--- services/api-v3/api/routes/dev.py
+++ services/api-v3/api/routes/dev.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
 from api.routes.dev_routes.interchange_data import interchange_router
+from fastapi import APIRouter
 
 dev_router = APIRouter(prefix="/dev", tags=["dev"])
 dev_router.include_router(interchange_router)
