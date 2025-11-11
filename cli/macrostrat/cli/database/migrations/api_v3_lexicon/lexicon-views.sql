eca66a64b882931369033cf9575078c7f49ccb9b
Daven Quinn <code@davenquinn.com>
2025-11-11 14:49:07 -0600

    Simplified filter definition route

--- cli/macrostrat/cli/database/migrations/api_v3_lexicon/lexicon-views.sql
+++ cli/macrostrat/cli/database/migrations/api_v3_lexicon/lexicon-views.sql
@@ -1,32 +1,41 @@
-
-
 /** Entities that can be used to filter columns */
-CREATE VIEW macrostrat_api.col_filter AS
-SELECT row_number() AS id,
-       combined.name,
-       combined.color,
-       combined.lex_id,
-       combined.type
-FROM ( SELECT liths.lith AS name,
-              liths.lith_color AS color,
-              liths.id AS lex_id,
-              'lithology'::text AS type
-       FROM macrostrat.liths
-       UNION ALL
-       SELECT strat_names.strat_name AS name,
-              NULL::character varying AS color,
-              strat_names.id AS lex_id,
-              'strat name'::text AS type
-       FROM macrostrat.strat_names
-       UNION ALL
-       SELECT intervals.interval_name AS name,
-              intervals.interval_color AS color,
-              intervals.id AS lex_id,
-              'interval'::text AS type
-       FROM macrostrat.intervals
-       UNION ALL
-       SELECT units.strat_name AS name,
-              NULL::character varying AS color,
-              units.id AS lex_id,
-              'unit'::text AS type
-       FROM macrostrat.units) combined;
+CREATE OR REPLACE VIEW macrostrat_api.col_filters AS
+SELECT
+  concat('lith:', l.id::text) AS uid,
+  l.lith AS name,
+  l.lith_color AS color,
+  l.id AS lex_id,
+  'lithology'::text AS type
+FROM macrostrat.liths l
+UNION ALL
+SELECT
+  concat('int:', i.id::text) AS uid,
+  i.interval_name AS name,
+  i.interval_color AS color,
+  i.id AS lex_id,
+  'interval'::text AS type
+FROM macrostrat.intervals i
+UNION ALL
+SELECT
+  concat('env:', e.id::text) AS uid,
+  e.environ AS name,
+  e.environ_color AS color,
+  e.id AS lex_id,
+  'environment'::text AS type
+FROM macrostrat.environs e
+UNION ALL
+SELECT
+  concat('concept:', c.concept_id::text) AS uid,
+  c.name AS name,
+  NULL::character varying AS color,
+  c.concept_id AS lex_id,
+  'concept'::text AS type
+FROM macrostrat.strat_names_meta c
+UNION ALL
+SELECT
+  concat('strat_name:', sn.id::text) AS uid,
+  sn.strat_name AS name,
+  NULL::character varying AS color,
+  sn.id AS lex_id,
+  'strat name'::text AS type
+FROM macrostrat.strat_names sn;
