bd21e640f3f9d420b92ba166a66268fcd350e866
Daven Quinn <code@davenquinn.com>
2025-11-20 21:21:35 -0600

    Start to deal with time windows

--- services/api-v3/api/match/__init__.py
+++ services/api-v3/api/match/__init__.py
@@ -1,5 +1,6 @@
+import enum
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi import APIRouter, Query
 from pydantic import BaseModel, Field
@@ -45,8 +46,18 @@ class MatchQuery(BaseModel):
         None, description="Search within a specific Macrostrat project"
     )
 
-    min_age: int = Field(0, description="Minimum age constraint in Ma")
-    max_age: int = Field(4540, description="Maximum age constraint in Ma")
+    b_age: int | None = Field(None, description="Early/lower age constraint in Ma")
+    t_age: int| None= Field(None, description="Late/upper age constraint in Ma")
+
+    b_interval: int | str | None = Field(
+        None, description="Early/lower interval name or ID"
+    )
+    t_interval: int | str | None = Field(
+        None, description="Late/upper interval name or ID"
+    )
+    interval: int | str | None = Field(
+        None, description="Interval name or ID to constrain matches"
+    )
 
     # Enforce one of col_id or lat/lng
     def validate(self):
@@ -56,19 +67,37 @@ class MatchQuery(BaseModel):
             raise ValueError("Either col_id or lat/lng must be provided.")
 
 
-class MatchWarning(BaseModel):
+class MatchMessageType(enum.Enum):
+    Info = "info"
+    Warning = "warning"
+    Error = "error"
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+class MatchMessage(BaseModel):
     message: str
-    details: str
+    details: Optional[str] = None
+    type: MatchMessageType = MatchMessageType.Info
 
     def __eq__(self, other):
-        if not isinstance(other, MatchWarning):
+        if not isinstance(other, MatchMessage):
             return NotImplemented
-        return self.message == other.message and self.details == other.details
+
+        return (
+            self.type == other.type
+            and self.message == other.message
+            and self.details == other.details
+        )
+
+    def __hash__(self):
+        return hash((self.type, self.message, self.details))
 
 
 class MatchData(MatchQuery):
     matches: list[MatchResult]
-    warnings: list[MatchWarning]
+    messages: list[MatchMessage]
 
 
 class MatchAPIResponse(BaseModel):
@@ -81,7 +110,7 @@ class MatchAPIResponse(BaseModel):
     comparison: MatchComparison = Field(
         ..., description="The type of string comparison that was performed."
     )
-    warnings: set[MatchWarning] | None = None
+    messages: set[MatchMessage] | None = None
 
 
 class MatchOptions(BaseModel):
@@ -105,7 +134,6 @@ def match_units(
     """
     Match stratigraphic name text to the Macrostrat lexicon and columns.
     """
-
     # Reconstruct separated mixins
     params = MatchQuery(**query.model_dump())
     opts = MatchOptions(**query.model_dump())
@@ -113,14 +141,12 @@ def match_units(
 
     db = get_database()
 
+    results = []
     match_data = build_match_data(db, params)
+    if match_data is not None:
+        results.append(match_data)
 
-    return MatchAPIResponse(
-        version="0.0.1",
-        date_accessed=datetime.now().isoformat(),
-        results=[match_data],
-        **opts.model_dump(),
-    )
+    return generate_response(results, opts)
 
 
 @router.post("/strat-names")
@@ -146,19 +172,30 @@ def match_units_multi(
 
     for params in body:
         match_data = build_match_data(db, params)
-        all_results.append(match_data)
+        if match_data is not None:
+            all_results.append(match_data)
 
+    return generate_response(all_results, opts)
+
+
+def generate_response(results: list[MatchData], opts: MatchOptions) -> MatchAPIResponse:
     # Aggregate warnings across all results
-    warnings: set[MatchWarning] = set()
-    for result in all_results:
-        for warning in result.warnings:
-            warnings.add(warning)
+    messages: set[MatchMessage] = set()
+    for result in results:
+        if result.messages is None:
+            continue
+        for msg in result.messages:
+            messages.add(msg)
+
+    # Could also raise an error/return a 404
+    if len(results) == 0:
+        messages.add(MatchMessage(message="No matches found"))
 
     return MatchAPIResponse(
         version="0.0.1",
         date_accessed=datetime.now().isoformat(),
-        results=all_results,
-        warnings=warnings,
+        results=results,
+        messages=messages,
         **opts.model_dump(),
     )
 
@@ -172,21 +209,25 @@ def build_match_data(db, params):
         col_ids = [col_id]
 
     results: list[MatchResult] = []
-    warnings: list[MatchWarning] = []
+    messages: list[MatchMessage] = []
     for col_id in col_ids:
         names = standardize_names(params.match_text)
         with db.engine.connect() as conn:
             rows = get_all_matched_units(conn, col_id, names)
         results += [MatchResult.from_row(r) for r in rows]
 
+    if len(results) == 0:
+        return None
+
     # TODO match based on lexicon footprints only if columns are not found.
 
     if len(col_ids) > 1:
-        warnings.append(
-            MatchWarning(
+        messages.append(
+            MatchMessage(
                 message="Multiple columns",
                 details="Results aggregated from multiple columns. Consider specifying a single col_id or project_id for more precise results.",
+                type=MatchMessageType.Warning,
             )
         )
 
-    return MatchData(**params.model_dump(), matches=results, warnings=warnings)
+    return MatchData(**params.model_dump(), matches=results, messages=messages)
--- services/api-v3/api/match/test_match_route.py
+++ services/api-v3/api/match/test_match_route.py
@@ -54,9 +54,10 @@ def test_no_match_units():
     data = response.json()
     assert "results" in data
     results = data["results"]
-    assert len(results) == 1
-    matches = results[0]["matches"]
-    assert len(matches) == 0
+    assert len(results) == 0
+    assert "messages" in data
+    messages = data["messages"]
+    assert any("No matches found" in msg["message"] for msg in messages)
 
 
 def test_multi_match_units():
@@ -113,3 +114,51 @@ def test_match_units_ambiguous_column():
     # Should match the unit from the higher priority project (1)
     assert best_match["unit_id"] == 34519
     assert best_match["strat_name_id"] == 5415
+
+
+pos = [-105.6, 40.9]  # Near front range of Colorado
+# Jelm Formation. unit id 15503, strat name 981
+# Triassic
+
+
+def test_match_units_time_limited():
+    response = client.get(
+        "/match/strat-names",
+        params={
+            "lat": pos[1],
+            "lng": pos[0],
+            "match_text": "Jelm Formation",
+            "max_age": 250.0,
+            "min_age": 200.0,
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert "results" in data
+    results = data["results"]
+    assert len(results) == 1
+    matches = results[0]["matches"]
+    assert len(matches) >= 1
+    best_match = matches[0]
+
+    assert best_match["unit_id"] == 15503
+    assert best_match["strat_name_id"] == 981
+
+
+def test_match_units_wrong_time_period():
+    response = client.get(
+        "/match/strat-names",
+        params={
+            "lat": pos[1],
+            "lng": pos[0],
+            "match_text": "Jelm Formation",
+            "b_age": 200.0,
+            "t_age": 100.0,
+        },
+    )
+    # TODO: should "no match" be a 404?
+    assert response.status_code == 200
+    data = response.json()
+    assert "results" in data
+    results = data["results"]
+    assert len(results) == 0
