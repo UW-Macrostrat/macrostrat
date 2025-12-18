a10adf44dfe10065cbb058a64f162c35989f11cd
amyfromandi <amyfromandi@gmail.com>
2025-12-18 11:55:44 -0600

    add sources to postgrest

--- cli/macrostrat/cli/database/migrations/ingest_preprocess_updates/ingest_preprocess_updates.sql
+++ cli/macrostrat/cli/database/migrations/ingest_preprocess_updates/ingest_preprocess_updates.sql
@@ -19,20 +19,47 @@ CREATE OR REPLACE VIEW maps.ingest_process AS
 SELECT * FROM maps_metadata.ingest_process;
 CREATE OR REPLACE VIEW macrostrat_api.map_ingest_metadata AS
 SELECT * FROM maps_metadata.ingest_process;
-
-
--- Make it writeable by users
 GRANT SELECT, UPDATE ON maps_metadata.ingest_process TO web_user;
 GRANT SELECT, UPDATE ON macrostrat_api.map_ingest_metadata TO web_user;
 
-create view macrostrat_api.map_ingest as
+--add views for postgrest
+create or replace view macrostrat_api.map_ingest as
     select * from maps_metadata.ingest_process;
-create view macrostrat_api.map_ingest_tags as
+create or replace view macrostrat_api.map_ingest_tags as
     select * from maps_metadata.ingest_process_tag;
+create or replace view macrostrat_api.maps_sources AS
+  select source_id,
+         name,
+         url,
+         ref_title,
+         authors,
+         ref_year,
+         ref_source,
+         isbn_doi,
+         scale,
+         license,
+         features,
+         area,
+         priority,
+         display_scales,
+         new_priority,
+         status_code,
+         slug,
+         raster_url,
+         scale_denominator,
+         is_finalized,
+         lines_oriented,
+         date_finalized,
+         ingested_by,
+         keywords,
+         language,
+         description
+  from maps.sources limit 50;
 
 GRANT SELECT, UPDATE ON macrostrat_api.map_ingest TO web_user, web_admin;
 GRANT SELECT, UPDATE ON macrostrat_api.map_ingest_tags TO web_user, web_admin;
-NOTIFY pgrst, 'reload schema';
+GRANT SELECT, UPDATE ON macrostrat_api.maps_sources TO web_user, web_admin;
+
 NOTIFY pgrst, 'reload schema';
 
 
