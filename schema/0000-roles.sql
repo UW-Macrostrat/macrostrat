8f635bf4ffde70436b2a9c4cdb21d8db1efa01b5
Daven Quinn <code@davenquinn.com>
2026-01-03 00:27:01 -0500

    Deepen migrations a bit for development deployment

--- schema/0000-roles.sql
+++ schema/0000-roles.sql
@@ -9,6 +9,18 @@ GRANT "macrostrat-admin" TO macrostrat_admin;
 CREATE ROLE macrostrat;
 
 -- Roles for auth/PostgREST system
+CREATE ROLE postgrest;
 CREATE ROLE web_admin;
 CREATE ROLE web_user;
 CREATE ROLE web_anon;
+
+GRANT web_admin TO macrostrat_admin;
+GRANT macrostrat TO postgres;
+GRANT web_user TO postgres;
+
+-- Role for read-only access from Rockd
+CREATE ROLE rockd_reader;
+GRANT rockd_reader TO "rockd-reader";
+GRANT "rockd-reader" TO rockd_reader;
+
+CREATE ROLE xdd_writer;
