33cd4173c39defc197220bb348db39f70e54dd28
Daven Quinn <code@davenquinn.com>
2026-01-03 14:36:54 -0500

    Updated schema management to better work with development db

--- schema/core/0000-globals.sql
+++ schema/core/0000-globals.sql
@@ -1,3 +1,15 @@
+-- General options modified from PGDump outputs
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', 'public', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS pgaudit WITH SCHEMA public;
--- schema/core/0003-hexgrids.sql
+++ schema/core/0003-hexgrids.sql
@@ -1,19 +1,4 @@
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-CREATE SCHEMA hexgrids;
-ALTER SCHEMA hexgrids OWNER TO macrostrat;
-SET default_tablespace = '';
-SET default_table_access_method = heap;
-
 CREATE TABLE hexgrids.bedrock_index (
     legend_id integer NOT NULL,
     hex_id integer NOT NULL,
