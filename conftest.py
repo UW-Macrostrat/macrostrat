a1927a1ab5d8cbf3cac8e2f929f6a6204755fa51
Daven Quinn <code@davenquinn.com>
2026-01-04 23:53:37 -0500

    Fixed path

--- conftest.py
+++ conftest.py
@@ -4,12 +4,11 @@ import importlib
 from pathlib import Path
 
 import docker
-from pytest import fixture, mark, skip
-from typer.testing import CliRunner
-
 from macrostrat.database import Database
 from macrostrat.dinosaur.upgrade_cluster import database_cluster
 from macrostrat.utils import get_logger, override_environment
+from pytest import fixture, mark, skip
+from typer.testing import CliRunner
 
 runner = CliRunner()
 
@@ -115,7 +114,7 @@ def load_config_module():
 
 @fixture()
 def cfg():
-    cfg_file = __here__ / "cli" / "tests" / "macrostrat.test.toml"
+    cfg_file = __here__ / "macrostrat" / "cli" / "tests" / "macrostrat.test.toml"
     with override_environment(MACROSTRAT_CONFIG=str(cfg_file), NO_COLOR="1"):
         mod_instance = load_config_module()
 
