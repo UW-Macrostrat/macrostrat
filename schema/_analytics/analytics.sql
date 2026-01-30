b7232c113299523ff53af3c952ffac103e10ce23
amyfromandi <amyfromandi@gmail.com>
2026-01-30 10:06:06 -0600

    added macrostrat permissions to tile table

--- schema/_analytics/analytics.sql
+++ schema/_analytics/analytics.sql
@@ -1,11 +1,11 @@
 --shows all the permissions of a given user and table
 SELECT
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'SELECT')   AS can_select,
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'INSERT')   AS can_insert,
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'UPDATE')   AS can_update,
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'DELETE')   AS can_delete,
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'TRUNCATE') AS can_truncate,
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'REFERENCES') AS can_references,
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'TRIGGER')  AS can_trigger,
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'SELECT')   AS can_select,
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'INSERT')   AS can_insert,
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'UPDATE')   AS can_update,
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'DELETE')   AS can_delete,
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'TRUNCATE') AS can_truncate,
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'REFERENCES') AS can_references,
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'TRIGGER')  AS can_trigger,
   has_schema_privilege('macrostrat', 'tile_cache', 'USAGE') AS can_use_schema,
-  has_table_privilege('macrostrat', 'tile_cache.tile_info', 'SELECT') AS macrostrat_can_select;
+  has_table_privilege('macrostrat', 'tile_cache.tile', 'SELECT') AS macrostrat_can_select;
