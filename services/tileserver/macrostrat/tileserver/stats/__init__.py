01150efcdf2772d3fad3c88b7935ecba5ae5ac58
Daven Quinn <code@davenquinn.com>
2026-06-27 03:01:54 -0500

    Refactor tileserver

--- services/tileserver/macrostrat/tileserver/stats/__init__.py
+++ services/tileserver/macrostrat/tileserver/stats/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from .usage import router as usage_stats_router
-from .requests import router as request_stats_router
+from .tiles import router as request_stats_router
 
 stats_router = APIRouter()
 stats_router.include_router(usage_stats_router, tags=["Web stats"], prefix="/web")
